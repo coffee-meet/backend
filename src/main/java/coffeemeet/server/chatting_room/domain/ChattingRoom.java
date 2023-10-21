75ac0c7c8b6c4c56419e97ca01d4a80e1b84cc6e
박은지 <qkrdmswl1018@naver.com>
2023-10-21 11:23:12 +0900

    feat: ChattingRoom 엔티티 작성

---
+++ src/main/java/coffeemeet/server/chatting_room/domain/ChattingRoom.java
@@ -0,0 +1,23 @@
+package coffeemeet.server.chatting_room.domain;
+
+import coffeemeet.server.common.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "chatting_rooms")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChattingRoom extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+}
