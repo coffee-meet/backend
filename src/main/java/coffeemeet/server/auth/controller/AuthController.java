18093ad09c7fe16c446ccd4a5c7faa008d38fb43
박은지 <qkrdmswl1018@naver.com>
2023-10-23 16:11:12 +0900

    feat: AuthController에 로그인 기능 구현 및 로그인 실패시 리다이렉트 로직 작성

--- src/main/java/coffeemeet/server/auth/controller/AuthController.java
+++ src/main/java/coffeemeet/server/auth/controller/AuthController.java
@@ -6,6 +6,7 @@ import coffeemeet.server.auth.utils.AuthTokens;
 import coffeemeet.server.user.domain.OAuthProvider;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,13 +30,25 @@ public class AuthController {
       HttpServletResponse response) throws IOException {
     String redirectUrl = authService.getAuthCodeRequestUrl(oAuthProvider);
     response.sendRedirect(redirectUrl);
-
     return new ResponseEntity<>(HttpStatus.FOUND);
   }
 
   @PostMapping("/sign-up")
   public ResponseEntity<AuthTokens> signup(@RequestBody SignupRequest request) {
     return ResponseEntity.ok(authService.signup(request));
+  }
+
+  @GetMapping("/login/{oAuthProvider}")
+  public ResponseEntity<?> login(@PathVariable OAuthProvider oAuthProvider,
+      @RequestParam String authCode, HttpServletResponse response) throws IOException {
+    Optional<AuthTokens> authTokens = authService.login(oAuthProvider, authCode);
+    if (authTokens.isPresent()) {
+      return ResponseEntity.ok(authTokens.get());
+    }
+
+    String signupUrl = "/oauth2/auth/sign-up";
+    response.sendRedirect(signupUrl);
+    return new ResponseEntity<>(HttpStatus.FOUND);
   }
 
 }
