3f88df225513046355eb65d8188dd0d33bc9f7ab
sangminnim <sangmins930@gmail.com>
2023-10-24 04:59:29 +0900

    feat: 인증 메일 보내기 Controller 구현

--- src/main/java/coffeemeet/server/certification/controller/CertificationController.java
+++ src/main/java/coffeemeet/server/certification/controller/CertificationController.java
@@ -1,14 +1,18 @@
 package coffeemeet.server.certification.controller;
 
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
+import coffeemeet.server.certification.dto.EmailDto;
 import coffeemeet.server.certification.service.CertificationService;
 import coffeemeet.server.common.annotation.Login;
 import coffeemeet.server.common.util.FileUtils;
 import coffeemeet.server.user.dto.AuthInfo;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -28,11 +32,22 @@ public class CertificationController {
       @Login
       AuthInfo authInfo,
       @RequestPart("businessCard")
-      @NotNull(message = "명함 이미지는 null일 수 없습니다.")
+      @NotNull
       MultipartFile businessCard
   ) {
     certificationService.uploadBusinessCard(authInfo.userId(),
         FileUtils.convertMultipartFileToFile(businessCard));
+  }
+
+  @PostMapping("users/company-mail")
+  @ResponseStatus(OK)
+  public void verifyMail(
+      @Login
+      AuthInfo authInfo,
+      @RequestBody @Valid
+      EmailDto emailDto
+  ) {
+    certificationService.verifyMail(authInfo.userId(), emailDto.companyEmail());
   }
 
 }
