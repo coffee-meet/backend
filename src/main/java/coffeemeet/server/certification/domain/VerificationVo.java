17604ff9df102e28f19833189117f4548bcd9d60
sangminnim <sangmins930@gmail.com>
2023-10-24 07:39:52 +0900

    refactor: class 이름 수정, 회사 이메일 필드 추가

--- src/main/java/coffeemeet/server/certification/domain/VerificationVo.java
+++ src/main/java/coffeemeet/server/certification/domain/VerificationVo.java
@@ -1,23 +1,26 @@
 package coffeemeet.server.certification.domain;
 
+import coffeemeet.server.user.domain.CompanyEmail;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 @Getter
-@RedisHash(value = "verification_code", timeToLive = 360)
-public class VerificationCode {
+@RedisHash(value = "verification_vo", timeToLive = 360)
+public class VerificationVo {
 
   @Id
   private Long userId;
+  private CompanyEmail companyEmail;
   private String code;
   private LocalDateTime createdAt;
 
-  public VerificationCode(Long userId, String code, LocalDateTime createdAt) {
+  public VerificationVo(Long userId, CompanyEmail companyEmail, String code,
+      LocalDateTime createdAt) {
     this.userId = userId;
+    this.companyEmail = companyEmail;
     this.code = code;
     this.createdAt = createdAt;
   }
-
 }
