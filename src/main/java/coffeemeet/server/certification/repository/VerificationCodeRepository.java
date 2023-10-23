222fc103d56a097359acbb301c15787e702e5d34
sangminnim <sangmins930@gmail.com>
2023-10-24 07:39:18 +0900

    refactor: Long으로 수정, class 이름 변경에 따른 수정

--- src/main/java/coffeemeet/server/certification/repository/VerificationCodeRepository.java
+++
@@ -1,8 +0,0 @@
-package coffeemeet.server.certification.repository;
-
-import coffeemeet.server.certification.domain.VerificationCode;
-import org.springframework.data.repository.CrudRepository;
-
-public interface VerificationCodeRepository extends CrudRepository<VerificationCode, String> {
-
-}
---
+++ src/main/java/coffeemeet/server/certification/repository/VerificationVoRepository.java
@@ -0,0 +1,8 @@
+package coffeemeet.server.certification.repository;
+
+import coffeemeet.server.certification.domain.VerificationVo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VerificationVoRepository extends CrudRepository<VerificationVo, Long> {
+
+}
