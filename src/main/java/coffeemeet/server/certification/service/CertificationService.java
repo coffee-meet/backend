e18bc6ee64a258c2a6c91763fb9888f93cfcf96c
sangminnim <sangmins930@gmail.com>
2023-10-24 04:59:35 +0900

    feat: 인증 메일 보내기 Service 구현

--- src/main/java/coffeemeet/server/certification/service/CertificationService.java
+++ src/main/java/coffeemeet/server/certification/service/CertificationService.java
@@ -1,9 +1,14 @@
 package coffeemeet.server.certification.service;
 
+import coffeemeet.server.certification.domain.VerificationCode;
+import coffeemeet.server.certification.repository.VerificationCodeRepository;
+import coffeemeet.server.common.media.EmailService;
 import coffeemeet.server.common.media.S3MediaService;
 import coffeemeet.server.common.util.FileUtils;
+import coffeemeet.server.user.domain.CompanyEmail;
 import coffeemeet.server.user.service.UserService;
 import java.io.File;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,14 +18,22 @@ public class CertificationService {
 
   private final S3MediaService s3MediaService;
   private final UserService userService;
-  private final BusinessCardS3KeyGenerator businessCardS3KeyGenerator;
+  private final EmailService emailService;
+  private final VerificationCodeRepository verificationCodeRepository;
 
   public void uploadBusinessCard(long userId, File file) {
-    String key = businessCardS3KeyGenerator.generate();
-
+    String key = s3MediaService.generateBusinessCardKey();
     s3MediaService.upload(key, file);
     userService.updateBusinessCardUrl(userId, s3MediaService.getUrl(key));
 
     FileUtils.delete(file);
   }
+
+  public void verifyMail(Long userId, String companyEmail) {
+    String verificationCode = emailService.generateVerificationCode();
+    emailService.sendVerificationMail(new CompanyEmail(companyEmail), verificationCode);
+    verificationCodeRepository.save(
+        new VerificationCode(userId, verificationCode, LocalDateTime.now()));
+  }
+
 }
