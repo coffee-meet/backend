89db54c5f74d9668b31224997a40043c97309fa9
yumyeonghan <mgtmh991013@gmail.com>
2023-10-23 18:33:55 +0900

    refactor: AuthController 수정
    - 회원가입 API 요청 데이터 검증 추가
    - 로그인 API 수정

--- src/main/java/coffeemeet/server/auth/controller/AuthController.java
+++ src/main/java/coffeemeet/server/auth/controller/AuthController.java
@@ -5,8 +5,8 @@ import coffeemeet.server.auth.service.AuthService;
 import coffeemeet.server.auth.utils.AuthTokens;
 import coffeemeet.server.user.domain.OAuthProvider;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import java.io.IOException;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -34,21 +34,14 @@ public class AuthController {
   }
 
   @PostMapping("/sign-up")
-  public ResponseEntity<AuthTokens> signup(@RequestBody SignupRequest request) {
+  public ResponseEntity<AuthTokens> signup(@Valid @RequestBody SignupRequest request) {
     return ResponseEntity.ok(authService.signup(request));
   }
 
   @GetMapping("/login/{oAuthProvider}")
-  public ResponseEntity<?> login(@PathVariable OAuthProvider oAuthProvider,
-      @RequestParam String authCode, HttpServletResponse response) throws IOException {
-    Optional<AuthTokens> authTokens = authService.login(oAuthProvider, authCode);
-    if (authTokens.isPresent()) {
-      return ResponseEntity.ok(authTokens.get());
-    }
-
-    String signupUrl = "/oauth2/auth/sign-up";
-    response.sendRedirect(signupUrl);
-    return new ResponseEntity<>(HttpStatus.FOUND);
+  public ResponseEntity<AuthTokens> login(@PathVariable OAuthProvider oAuthProvider,
+      @RequestParam String authCode) {
+    return ResponseEntity.ok(authService.login(oAuthProvider, authCode));
   }
 
 }
