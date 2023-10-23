bd6f61da852e2700fc2aac6b189cc4805348adad
박은지 <qkrdmswl1018@naver.com>
2023-10-23 23:35:19 +0900

    feat: 프로필 DTO 작성

---
+++ src/main/java/coffeemeet/server/user/dto/MyProfileResponse.java
@@ -0,0 +1,43 @@
+package coffeemeet.server.user.dto;
+
+import coffeemeet.server.interest.domain.Interest;
+import coffeemeet.server.interest.domain.Keyword;
+import coffeemeet.server.user.domain.User;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record MyProfileResponse(
+    String name,
+    String nickname,
+    String email,
+    String profileImageUrl,
+    String birthYear,
+    String birthDay,
+    int reportedCount,
+    LocalDateTime sanctionPeriod,
+    String companyEmail,
+    String department,
+    List<Keyword> interests
+) {
+
+  public static MyProfileResponse from(User user, List<Interest> interests) {
+    List<Keyword> keywords = interests.stream()
+        .map(Interest::getKeyword)
+        .toList();
+
+    return new MyProfileResponse(
+        user.getProfile().getName(),
+        user.getProfile().getNickname(),
+        user.getProfile().getEmail().getEmail(),
+        user.getProfile().getProfileImageUrl(),
+        user.getProfile().getBirth().getYear(),
+        user.getProfile().getBirth().getDay(),
+        user.getReportInfo().getReportedCount(),
+        user.getReportInfo().getSanctionPeriod(),
+        user.getCertification().getCompanyEmail().getCompanyEmail(),
+        user.getCertification().getDepartment(),
+        keywords
+    );
+  }
+
+}
