0fc7cdd3c3146e3a99d5195254c949658735af4f
박은지 <qkrdmswl1018@naver.com>
2023-10-24 15:01:18 +0900

    feat: 관심사 업데이트 기능 구현

--- src/main/java/coffeemeet/server/interest/domain/Keyword.java
+++ src/main/java/coffeemeet/server/interest/domain/Keyword.java
@@ -4,5 +4,15 @@ public enum Keyword {
   COOK,
   GAME,
   EXERCISE,
+  ;
+
+  public static boolean isValidKeyword(String keyword) {
+    for (Keyword key : Keyword.values()) {
+      if (key.name().equals(keyword)) {
+        return true;
+      }
+    }
+    return false;
+  }
 
 }
---
+++ src/main/java/coffeemeet/server/interest/service/InterestService.java
@@ -0,0 +1,49 @@
+package coffeemeet.server.interest.service;
+
+import coffeemeet.server.interest.domain.Interest;
+import coffeemeet.server.interest.domain.Keyword;
+import coffeemeet.server.interest.repository.InterestRepository;
+import coffeemeet.server.user.domain.User;
+import coffeemeet.server.user.repository.UserRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class InterestService {
+
+  private static final int MAX_INTERESTS = 3;
+
+  private final InterestRepository interestRepository;
+  private final UserRepository userRepository;
+
+  @Transactional
+  public void updateInterests(Long userId, List<Keyword> interests) {
+    validateInterests(interests);
+
+    for (Keyword interest : interests) {
+      if (!Keyword.isValidKeyword(interest.name())) {
+        throw new IllegalArgumentException("유효한 관심사가 아닙니다.");
+      }
+    }
+    User user = userRepository.findById(userId)
+        .orElseThrow(() -> new IllegalArgumentException("해당 사용자를 찾을 수 없습니다."));
+
+    List<Interest> currentInterests = interestRepository.findAllByUserId(userId);
+    interestRepository.deleteAll(currentInterests);
+
+    for (Keyword keyword : interests) {
+      Interest newInterest = new Interest(keyword, user);
+      interestRepository.save(newInterest);
+    }
+  }
+
+  private void validateInterests(List<Keyword> interests) {
+    if (interests.isEmpty() || interests.size() > MAX_INTERESTS) {
+      throw new IllegalArgumentException("최소 1개, 최대 3개의 관심사를 선택하세요.");
+    }
+  }
+
+}
