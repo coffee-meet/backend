3ff17480355700035ca59e90620a153783aab30c
sangminnim <sangmins930@gmail.com>
2023-10-24 17:14:58 +0900

    refactor: 컨벤션에 맞춤

--- src/main/java/coffeemeet/server/certification/controller/CertificationController.java
+++ src/main/java/coffeemeet/server/certification/controller/CertificationController.java
@@ -1,8 +1,5 @@
 package coffeemeet.server.certification.controller;
 
-import static org.springframework.http.HttpStatus.CREATED;
-import static org.springframework.http.HttpStatus.OK;
-
 import coffeemeet.server.certification.dto.EmailDto;
 import coffeemeet.server.certification.dto.VerificationCodeDto;
 import coffeemeet.server.certification.service.CertificationService;
@@ -12,11 +9,11 @@ import coffeemeet.server.user.dto.AuthInfo;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -28,37 +25,39 @@ public class CertificationController {
   private final CertificationService certificationService;
 
   @PostMapping("/users/business-card")
-  @ResponseStatus(CREATED)
-  public void uploadBusinessCard(
+  public ResponseEntity<Void> uploadBusinessCard(
       @Login
       AuthInfo authInfo,
-      @RequestPart("businessCard")
       @NotNull
+      @RequestPart("businessCard")
       MultipartFile businessCard
   ) {
     certificationService.uploadBusinessCard(authInfo.userId(),
         FileUtils.convertMultipartFileToFile(businessCard));
+    return ResponseEntity.ok().build();
   }
 
   @PostMapping("/users/company-mail")
-  @ResponseStatus(OK)
-  public void sendVerificationMail(
+  public ResponseEntity<Void> sendVerificationCodeByEmail(
       @Login
       AuthInfo authInfo,
-      @RequestBody @Valid
+      @Valid @RequestBody
       EmailDto emailDto
   ) {
     certificationService.sendVerificationMail(authInfo.userId(), emailDto.companyEmail());
+    return ResponseEntity.ok().build();
+
   }
 
   @PostMapping("/users/company-mail/verification")
-  public void verifyEmail(
+  public ResponseEntity<Void> verifyEmail(
       @Login
       AuthInfo authInfo,
-      @RequestBody @Valid
+      @Valid @RequestBody
       VerificationCodeDto verificationCodeDto
   ) {
     certificationService.verifyEmail(authInfo.userId(), verificationCodeDto.verificationCode());
+    return ResponseEntity.ok().build();
   }
 
 }
