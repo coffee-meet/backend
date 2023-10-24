9ec957ba48ddd6a7c48f12013874cba72c01fd92
sangminnim <sangmins930@gmail.com>
2023-10-24 17:24:51 +0900

    refactor: 셍성자 파라미터에 LocalDateTime 삭제, 클래스명 변경

--- src/main/java/coffeemeet/server/certification/repository/VerificationVoRepository.java
+++ src/main/java/coffeemeet/server/certification/repository/VerificationVoRepository.java
@@ -1,8 +1,8 @@
 package coffeemeet.server.certification.repository;
 
-import coffeemeet.server.certification.domain.VerificationVo;
+import coffeemeet.server.certification.domain.EmailVerification;
 import org.springframework.data.repository.CrudRepository;
 
-public interface VerificationVoRepository extends CrudRepository<VerificationVo, Long> {
+public interface VerificationVoRepository extends CrudRepository<EmailVerification, Long> {
 
 }
