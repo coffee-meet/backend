76034244fa91a6bd19019718bb275d9884e30e20
yumyeonghan <mgtmh991013@gmail.com>
2023-10-25 16:57:11 +0900

    feat: 프로필 수정 API 작성

--- src/main/java/coffeemeet/server/user/controller/UserController.java
+++ src/main/java/coffeemeet/server/user/controller/UserController.java
@@ -1,13 +1,14 @@
 package coffeemeet.server.user.controller;
 
 import coffeemeet.server.common.annotation.Login;
+import coffeemeet.server.common.util.FileUtils;
 import coffeemeet.server.user.dto.AuthInfo;
 import coffeemeet.server.user.dto.MyProfileResponse;
-import coffeemeet.server.user.dto.UpdateProfileImageUrlRequest;
 import coffeemeet.server.user.dto.UpdateProfileRequest;
 import coffeemeet.server.user.dto.UserProfileResponse;
 import coffeemeet.server.user.service.UserService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +16,9 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -35,18 +38,22 @@ public class UserController {
   }
 
   @PatchMapping("/me/profile-image")
-  public ResponseEntity<Void> updateProfileImage(@Login AuthInfo authInfo,
-      @Valid @RequestBody UpdateProfileImageUrlRequest request) {
-    userService.updateProfileImage(authInfo.userId(), request.profileImageUrl());
-    return ResponseEntity.noContent().build();
+  public ResponseEntity<Void> updateProfileImage(
+      @Login AuthInfo authInfo,
+      @NotNull
+      @RequestPart("profileImage")
+      MultipartFile profileImage) {
+    userService.updateProfileImage(
+        authInfo.userId(),
+        FileUtils.convertMultipartFileToFile(profileImage));
+    return ResponseEntity.ok().build();
   }
 
   @PatchMapping("/me")
   public ResponseEntity<Void> updateProfileInfo(@Login AuthInfo authInfo,
       @Valid @RequestBody UpdateProfileRequest request) {
-    userService.updateProfileInfo(authInfo.userId(), request.nickname(), request.name(),
-        request.interests());
-    return ResponseEntity.noContent().build();
+    userService.updateProfileInfo(authInfo.userId(), request.nickname(), request.interests());
+    return ResponseEntity.ok().build();
   }
 
 }
