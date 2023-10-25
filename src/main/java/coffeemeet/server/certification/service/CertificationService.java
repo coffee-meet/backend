ff0caafeccc327b07229f62900b6044a04b3bb11
sangminnim <sangmins930@gmail.com>
2023-10-26 04:13:47 +0900

    refactor: 다른 도메인의 서비스 의존이 아닌 Repository 가공 클래스 의존으로 변경

--- src/main/java/coffeemeet/server/certification/service/CertificationService.java
+++ src/main/java/coffeemeet/server/certification/service/CertificationService.java
@@ -2,63 +2,82 @@ package coffeemeet.server.certification.service;
 
 import static coffeemeet.server.common.media.S3MediaService.KeyType.BUSINESS_CARD;
 
+import coffeemeet.server.certification.domain.CompanyEmail;
+import coffeemeet.server.certification.domain.Department;
 import coffeemeet.server.certification.domain.EmailVerification;
 import coffeemeet.server.certification.repository.VerificationVoRepository;
+import coffeemeet.server.certification.service.cq.CertificationCommand;
+import coffeemeet.server.certification.service.cq.CertificationQuery;
 import coffeemeet.server.common.media.EmailService;
 import coffeemeet.server.common.media.S3MediaService;
 import coffeemeet.server.common.util.FileUtils;
-import coffeemeet.server.user.domain.CompanyEmail;
+import coffeemeet.server.user.domain.User;
 import coffeemeet.server.user.service.UserService;
 import java.io.File;
 import java.util.random.RandomGenerator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class CertificationService {
 
-  public static final String VERIFICATION_CODE_NOT_FOUND = "인증코드 기간이 만료되었거나 해당 유저가 인증 번호를 요청한 기록이 없습니다.";
-  public static final String WRONG_VERIFICATION_CODE = "잘못된 인증코드입니다.";
+  private static final String VERIFICATION_CODE_NOT_FOUND = "인증코드 기간이 만료되었거나 해당 유저가 인증 번호를 요청한 기록이 없습니다.";
+  private static final String WRONG_VERIFICATION_CODE = "잘못된 인증코드입니다.";
   private static final RandomGenerator RANDOM_GENERATOR = RandomGenerator.getDefault();
 
   private final S3MediaService s3MediaService;
-  private final UserService userService;
   private final EmailService emailService;
+  private final UserService userService;
   private final VerificationVoRepository verificationVoRepository;
+  private final CertificationQuery certificationQuery;
+  private final CertificationCommand certificationCommand;
 
-  public void uploadBusinessCard(long userId, File file) {
+  @Transactional
+  public void registerCertification(long userId, String email, String departmentName,
+      File businessCardImage) {
     String key = s3MediaService.generateKey(BUSINESS_CARD);
-    s3MediaService.upload(key, file);
-    userService.updateBusinessCardUrl(userId, s3MediaService.getUrl(key));
+    uploadBusinessCard(userId, key, businessCardImage);
 
-    FileUtils.delete(file);
+    CompanyEmail companyEmail = new CompanyEmail(email);
+    String businessCardUrl = s3MediaService.getUrl(key);
+    Department department = Department.valueOf(departmentName);
+    User user = userService.getUserById(userId);
+    certificationCommand.newCertification(companyEmail, businessCardUrl, department, user);
   }
 
+  private void uploadBusinessCard(long userId, String key, File businessCardUrl) {
+    certificationCommand.applyIfCertifiedUser(userId, certification -> {
+      String oldKey = s3MediaService.extractKey(certification.getBusinessCardUrl(), BUSINESS_CARD);
+      s3MediaService.delete(oldKey);
+    });
+
+    s3MediaService.upload(key, businessCardUrl);
+    FileUtils.delete(businessCardUrl);
+  }
+
+  @Transactional
   public void sendVerificationMail(Long userId, String email) {
     CompanyEmail companyEmail = new CompanyEmail(email);
-    userService.validateDuplicatedCompanyEmail(companyEmail);
+    certificationQuery.checkDuplicatedCompanyEmail(companyEmail);
 
     String verificationCode = generateVerificationCode();
     emailService.sendVerificationCode(companyEmail, verificationCode);
-    verificationVoRepository.save(
-        new EmailVerification(userId, companyEmail, verificationCode));
+    verificationVoRepository.save(new EmailVerification(userId, companyEmail, verificationCode));
+  }
+
+  @Transactional(readOnly = true)
+  public void verifyEmail(Long userId, String verificationCode) {
+    EmailVerification emailVerification = verificationVoRepository.findById(userId)
+        .orElseThrow(() -> new IllegalArgumentException(VERIFICATION_CODE_NOT_FOUND));
+    if (!emailVerification.getCode().equals(verificationCode)) {
+      throw new IllegalArgumentException(WRONG_VERIFICATION_CODE);
+    }
   }
 
   private String generateVerificationCode() {
     return String.format("%06d", RANDOM_GENERATOR.nextInt(1000000));
-  }
-
-  public void verifyEmail(Long userId, String verificationCode) {
-    EmailVerification emailVerification = verificationVoRepository.findById(userId)
-        .orElseThrow(
-            () -> new IllegalArgumentException(VERIFICATION_CODE_NOT_FOUND));
-
-    if (!emailVerification.getCode().equals(verificationCode)) {
-      throw new IllegalArgumentException(WRONG_VERIFICATION_CODE);
-    }
-
-    userService.updateCompanyEmail(userId, emailVerification.getCompanyEmail());
   }
 
 }
--- src/main/java/coffeemeet/server/user/service/UserService.java
+++ src/main/java/coffeemeet/server/user/service/UserService.java
@@ -3,12 +3,13 @@ package coffeemeet.server.user.service;
 import static coffeemeet.server.common.media.S3MediaService.KeyType.PROFILE_IMAGE;
 
 import coffeemeet.server.auth.dto.OAuthInfoResponse;
+import coffeemeet.server.certification.domain.Certification;
+import coffeemeet.server.certification.service.cq.CertificationQuery;
 import coffeemeet.server.common.media.S3MediaService;
 import coffeemeet.server.interest.domain.Interest;
 import coffeemeet.server.interest.domain.Keyword;
 import coffeemeet.server.interest.repository.InterestRepository;
 import coffeemeet.server.interest.service.InterestService;
-import coffeemeet.server.user.domain.CompanyEmail;
 import coffeemeet.server.user.domain.User;
 import coffeemeet.server.user.dto.MyProfileResponse;
 import coffeemeet.server.user.dto.UserProfileResponse;
@@ -24,44 +25,29 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class UserService {
 
-  private static final String EXISTED_COMPANY_EMAIL_ERROR = "이미 사용 중인 회사이메일입니다.";
   private static final String ALREADY_REGISTERED_MESSAGE = "이미 가입된 사용자입니다.";
 
   private final S3MediaService s3MediaService;
   private final UserRepository userRepository;
   private final InterestRepository interestRepository;
   private final InterestService interestService;
+  private final CertificationQuery certificationQuery;
 
-  @Transactional
-  public void updateBusinessCardUrl(Long userId, String businessCardUrl) {
-    User user = getUserById(userId);
-    user.updateBusinessCardUrl(businessCardUrl);
-  }
-
-  public void validateDuplicatedCompanyEmail(CompanyEmail companyEmail) {
-    if (userRepository.existsByCertification_CompanyEmail(companyEmail)) {
-      throw new IllegalArgumentException(EXISTED_COMPANY_EMAIL_ERROR);
-    }
-  }
-
-  public UserProfileResponse findUserProfile(String nickname) {
-    User user = userRepository.findUserByProfileNickname(nickname)
+  public UserProfileResponse findUserProfile(long userId) {
+    User user = userRepository.findById(userId)
         .orElseThrow(() -> new IllegalArgumentException("해당 사용자를 찾을 수 없습니다."));
+    List<Interest> interests = interestRepository.findAllByUserId(userId);
+    Certification certification = certificationQuery.getCertificationByUserId(userId);
 
-    List<Interest> interests = interestRepository.findAllByUserId(user.getId());
-    return UserProfileResponse.of(user, interests);
+    return UserProfileResponse.of(user, certification.getDepartment(), interests);
   }
 
   public MyProfileResponse findMyProfile(Long userId) {
     User user = getUserById(userId);
     List<Interest> interests = interestRepository.findAllByUserId(userId);
-    return MyProfileResponse.of(user, interests);
-  }
+    Certification certification = certificationQuery.getCertificationByUserId(userId);
 
-  @Transactional
-  public void updateCompanyEmail(Long userId, CompanyEmail companyEmail) {
-    User user = getUserById(userId);
-    user.updateCompanyEmail(companyEmail);
+    return MyProfileResponse.of(user, interests, certification.getDepartment());
   }
 
   @Transactional
@@ -111,7 +97,7 @@ public class UserService {
     }
   }
 
-  private User getUserById(Long userId) {
+  public User getUserById(Long userId) {
     return userRepository.findById(userId)
         .orElseThrow(IllegalArgumentException::new);
   }
