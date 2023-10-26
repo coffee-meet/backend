fa2e73d7df19b955a26d6802aebb9e29e953f842
sangminnim <sangmins930@gmail.com>
2023-10-26 16:00:25 +0900

    refactor: NonNull 추가

--- src/main/java/coffeemeet/server/certification/domain/CompanyEmail.java
+++ src/main/java/coffeemeet/server/certification/domain/CompanyEmail.java
@@ -1,11 +1,11 @@
 package coffeemeet.server.certification.domain;
 
-import coffeemeet.server.common.util.Patterns;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Getter
 @Embeddable
@@ -15,15 +15,8 @@ public class CompanyEmail {
   @Column(name = "company_email")
   private String value;
 
-  public CompanyEmail(String value) {
-    validateCompanyEmail(value);
+  public CompanyEmail(@NonNull String value) {
     this.value = value;
-  }
-
-  private void validateCompanyEmail(String companyEmail) {
-    if (!Patterns.EMAIL_PATTERN.matcher(companyEmail).matches()) {
-      throw new IllegalArgumentException("올바르지 않은 이메일입니다.");
-    }
   }
 
 }
