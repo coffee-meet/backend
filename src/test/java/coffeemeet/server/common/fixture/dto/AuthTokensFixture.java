07109a2319894bc1d119cc4835d0340142f1b3da
yumyeonghan <mgtmh991013@gmail.com>
2023-10-27 18:29:23 +0900

    test: add authTokens fixture

--- src/test/java/coffeemeet/server/common/fixture/dto/AuthTokensFixture.java
+++ src/test/java/coffeemeet/server/common/fixture/dto/AuthTokensFixture.java
@@ -1,5 +1,7 @@
 package coffeemeet.server.common.fixture.dto;
 
+import static org.instancio.Select.field;
+
 import coffeemeet.server.auth.domain.AuthTokens;
 import org.instancio.Instancio;
 
@@ -7,6 +9,12 @@ public class AuthTokensFixture {
 
   public static AuthTokens authTokens() {
     return Instancio.of(AuthTokens.class)
+        .create();
+  }
+
+  public static AuthTokens authTokens(String refreshToken) {
+    return Instancio.of(AuthTokens.class)
+        .set(field(AuthTokens::refreshToken), refreshToken)
         .create();
   }
 
