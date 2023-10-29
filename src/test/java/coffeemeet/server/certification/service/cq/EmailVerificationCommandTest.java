f41885c048f65233191443c362473fa505ab75e4
sangminnim <sangmins930@gmail.com>
2023-10-30 06:44:28 +0900

    test: EmailVerificationCommand Test

---
+++ src/test/java/coffeemeet/server/certification/service/cq/EmailVerificationCommandTest.java
@@ -0,0 +1,42 @@
+package coffeemeet.server.certification.service.cq;
+
+import static coffeemeet.server.common.fixture.entity.CertificationFixture.emailVerification;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.only;
+
+import coffeemeet.server.certification.domain.EmailVerification;
+import coffeemeet.server.certification.repository.EmailVerificationRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class EmailVerificationCommandTest {
+
+  @InjectMocks
+  private EmailVerificationCommand emailVerificationCommand;
+  @Mock
+  private EmailVerificationRepository emailVerificationRepository;
+
+  @Test
+  @DisplayName("새로운 EmailVerification 객체를 저장할 수 있다.")
+  void newEmailVerificationTest() {
+    // given
+    EmailVerification emailVerification = emailVerification();
+    given(emailVerificationRepository.save(any(EmailVerification.class))).willReturn(
+        emailVerification);
+
+    // when
+    emailVerificationCommand.newEmailVerification(emailVerification.getUserId(),
+        emailVerification.getCompanyEmail(), emailVerification.getCode());
+
+    // then
+    then(emailVerificationRepository).should(only()).save(any(EmailVerification.class));
+  }
+
+}
