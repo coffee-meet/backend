7a4e40ea092bf182b839d2f48ed0fc3d9b004574
yumyeonghan <mgtmh991013@gmail.com>
2023-10-30 18:24:19 +0900

    feat: GlobalErrorCode 작성

---
+++ src/main/java/coffeemeet/server/common/execption/exception/GlobalErrorCode.java
@@ -0,0 +1,26 @@
+package coffeemeet.server.common.execption.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum GlobalErrorCode implements ErrorCode {
+
+  VALIDATION_ERROR("G000", "유효하지 않은 입력입니다."),
+  INTERNAL_SERVER_ERROR("G050", "예상치 못한 오류입니다.");
+
+  private final String code;
+  private final String message;
+
+  @Override
+  public String code() {
+    return this.code;
+  }
+
+  @Override
+  public String message() {
+    return this.message;
+  }
+
+}
