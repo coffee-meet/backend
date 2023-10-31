326b36891a0cec13b8bf856094339e435649366d
yumyeonghan <mgtmh991013@gmail.com>
2023-10-31 15:09:43 +0900

    feat: CERTIFICATION_NOT_FOUND 에러코드 추가

--- src/main/java/coffeemeet/server/certification/exception/CertificationErrorCode.java
+++ src/main/java/coffeemeet/server/certification/exception/CertificationErrorCode.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum CertificationErrorCode implements ErrorCode {
   EXISTED_COMPANY_EMAIL("C000", "이메일이 중복됩니다."),
+  CERTIFICATION_NOT_FOUND("C004", "인증 정보를 찾을 수 없습니다."),
   INVALID_VERIFICATION_CODE("C004", "잘못된 인증코드입니다.");
 
   private final String errorCode;
