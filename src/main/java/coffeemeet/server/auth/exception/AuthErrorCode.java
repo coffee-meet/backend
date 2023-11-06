c87fd408824708377eb225537d9d88315d400813
yumyeonghan <mgtmh991013@gmail.com>
2023-11-06 20:14:10 +0900

    feat: AuthErrorCode 추가

--- src/main/java/coffeemeet/server/auth/exception/AuthErrorCode.java
+++ src/main/java/coffeemeet/server/auth/exception/AuthErrorCode.java
@@ -10,6 +10,7 @@ public enum AuthErrorCode implements ErrorCode {
   INVALID_LOGIN_TYPE("A000", "지원하지 않는 로그인 타입입니다."),
   AUTHENTICATION_FAILED("A001", "인증이 실패했습니다."),
   AUTHORIZATION_FAILED("A003", "인가에 실패했습니다."),
+  HEADER_NOT_FOUND("A004", "헤더에 인증 코드가 없습니다."),
   ALREADY_REGISTERED("A009", "이미 가입된 사용자입니다."),
   DELETED_USER("A010", "탈퇴한 지 30일이 지난 사용자입니다. 다시 회원가입해 주세요.");
 
