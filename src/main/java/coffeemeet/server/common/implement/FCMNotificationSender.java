6b2031af78ba2abacb1c1fb38bcd17532d142b35
sangminnim <sangmins930@gmail.com>
2023-11-07 17:09:29 +0900

    feat: 알림 보내기 구현

---
+++ src/main/java/coffeemeet/server/common/implement/FCMNotificationSender.java
@@ -0,0 +1,80 @@
+package coffeemeet.server.common.implement;
+
+import static coffeemeet.server.common.execption.GlobalErrorCode.INVALID_FCM_TOKEN;
+import static coffeemeet.server.common.execption.GlobalErrorCode.PUSH_NOTIFICATION_SEND_FAILURE;
+import static com.google.firebase.messaging.MessagingErrorCode.INVALID_ARGUMENT;
+import static com.google.firebase.messaging.MessagingErrorCode.UNREGISTERED;
+
+import coffeemeet.server.common.execption.InvalidNotificationTokenException;
+import coffeemeet.server.common.execption.NotificationFailException;
+import coffeemeet.server.user.domain.NotificationInfo;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.MessagingErrorCode;
+import com.google.firebase.messaging.MulticastMessage;
+import com.google.firebase.messaging.Notification;
+import java.util.Set;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class FCMNotificationSender {
+
+  private final FirebaseMessaging firebaseMessaging;
+  private static final String INVALID_FCM_TOKEN_MESSAGE = "사용된 토큰(%s)이 유효하지 않습니다.";
+  private static final String PUSH_NOTIFICATION_SEND_FAILURE_MESSAGE = "푸시 알림 전송에 실패했습니다. 토큰: %s";
+
+  public void sendNotificationByToken(NotificationInfo notificationInfo, String content) {
+    if (!notificationInfo.isSubscribedToNotification()) {
+      return;
+    }
+
+    Notification notification = createNotification(content);
+    Message message = Message.builder().setToken(notificationInfo.getToken())
+        .setNotification(notification)
+        .build();
+
+    try {
+      firebaseMessaging.send(message);
+    } catch (FirebaseMessagingException e) {
+      handleFirebaseMessagingException(e, notificationInfo.getToken());
+    }
+  }
+
+  public void sendNotificationByTokens(Set<NotificationInfo> notificationInfos, String content) {
+    notificationInfos.removeIf(notificationInfo -> !notificationInfo.isSubscribedToNotification());
+
+    Set<String> tokens = notificationInfos.stream().map(
+        NotificationInfo::getToken
+    ).collect(Collectors.toUnmodifiableSet());
+
+    Notification notification = createNotification(content);
+    MulticastMessage message = MulticastMessage.builder().addAllTokens(tokens)
+        .setNotification(notification).build();
+
+    try {
+      firebaseMessaging.sendMulticast(message);
+    } catch (FirebaseMessagingException e) {
+      handleFirebaseMessagingException(e, String.join(", ", tokens)); //
+    }
+  }
+
+  private void handleFirebaseMessagingException(FirebaseMessagingException e, String token) {
+    MessagingErrorCode messagingErrorCode = e.getMessagingErrorCode();
+
+    if (messagingErrorCode == UNREGISTERED || messagingErrorCode == INVALID_ARGUMENT) {
+      throw new InvalidNotificationTokenException(INVALID_FCM_TOKEN,
+          String.format(INVALID_FCM_TOKEN_MESSAGE, token));
+    }
+    throw new NotificationFailException(PUSH_NOTIFICATION_SEND_FAILURE,
+        String.format(PUSH_NOTIFICATION_SEND_FAILURE_MESSAGE, token));
+  }
+
+  private Notification createNotification(String content) {
+    return Notification.builder().setTitle("커피밋").setBody(content).build();
+  }
+
+}
