d4f8ed8426429259171c37b3d9b0e13ebb54ade0
sangminnim <sangmins930@gmail.com>
2023-11-07 17:07:40 +0900

    test: 알림 픽스처 구현

--- src/test/java/coffeemeet/server/common/fixture/entity/UserFixture.java
+++ src/test/java/coffeemeet/server/common/fixture/entity/UserFixture.java
@@ -1,22 +1,22 @@
 package coffeemeet.server.common.fixture.entity;
 
+import static java.time.LocalDateTime.now;
 import static org.instancio.Select.field;
 
 import coffeemeet.server.user.domain.Birth;
 import coffeemeet.server.user.domain.Email;
+import coffeemeet.server.user.domain.NotificationInfo;
 import coffeemeet.server.user.domain.Profile;
 import coffeemeet.server.user.domain.User;
 import coffeemeet.server.user.presentation.dto.NotificationTokenHTTP;
+import java.util.Set;
 import org.instancio.Instancio;
 
 public class UserFixture {
 
   public static User user() {
-    return Instancio.of(User.class)
-        .set(field(User::getProfile), profile())
-        .ignore(field(User::isDeleted))
-        .ignore(field(User::getChattingRoom))
-        .create();
+    return Instancio.of(User.class).set(field(User::getProfile), profile())
+        .ignore(field(User::isDeleted)).ignore(field(User::getChattingRoom)).create();
   }
 
   private static Birth birth() {
@@ -27,11 +27,9 @@ public class UserFixture {
   }
 
   private static Profile profile() {
-    return Instancio.of(Profile.class)
-        .set(field(Profile::getBirth), birth())
+    return Instancio.of(Profile.class).set(field(Profile::getBirth), birth())
         .set(field(Profile::getEmail), new Email("test123@gmail.com"))
-        .generate(field(Profile::getNickname), gen -> gen.string().maxLength(20))
-        .create();
+        .generate(field(Profile::getNickname), gen -> gen.string().maxLength(20)).create();
   }
 
   public static NotificationTokenHTTP.Request notificationTokenHTTPRequest() {
@@ -39,6 +37,24 @@ public class UserFixture {
   }
 
   public static String token() {
+    return Instancio.create(String.class);
+  }
+
+  public static NotificationInfo notificationInfo() {
+    return Instancio.of(NotificationInfo.class)
+        .set(field(NotificationInfo::isSubscribedToNotification), true)
+        .generate(field(NotificationInfo::getCreatedTokenAt),
+            gen -> gen.temporal().localDateTime().range(now().minusMonths(2), now())).create();
+  }
+
+  public static Set<NotificationInfo> notificationInfos() {
+    return Instancio.ofSet(NotificationInfo.class)
+        .set(field(NotificationInfo::isSubscribedToNotification), true)
+        .generate(field(NotificationInfo::getCreatedTokenAt),
+            gen -> gen.temporal().localDateTime().range(now().minusMonths(2), now())).create();
+  }
+
+  public static String content() {
     return Instancio.create(String.class);
   }
 
