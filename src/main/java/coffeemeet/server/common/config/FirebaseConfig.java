02dc731a3870d922c34fbd1c54fbb08aec3223ad
sangminnim <sangmins930@gmail.com>
2023-11-08 17:09:19 +0900

    refactor: 위치 변경

--- src/main/java/coffeemeet/server/common/config/FirebaseConfig.java
+++ src/main/java/coffeemeet/server/common/config/FirebaseConfig.java
@@ -4,21 +4,21 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
-import java.io.FileInputStream;
 import java.io.IOException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 public class FirebaseConfig {
 
   @Bean
   FirebaseApp firebaseApp() throws IOException {
-    FileInputStream serviceAccount =
-        new FileInputStream("/home/ubuntu/firebase-service-key.json");
+    String filename = "firebase-service-key.json";
+    ClassPathResource resource = new ClassPathResource(filename);
 
     FirebaseOptions options = FirebaseOptions.builder()
-        .setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
+        .setCredentials(GoogleCredentials.fromStream(resource.getInputStream())).build();
 
     return FirebaseApp.initializeApp(options);
   }
