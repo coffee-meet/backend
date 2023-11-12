95ee4f472b8594adbbfe3043a10c1a9d5fad8818
yumyeonghan <mgtmh991013@gmail.com>
2023-11-13 01:08:52 +0900

    feat: AuthenticationInterceptor 인터셉터 작성
    - Authorization 헤더의 토큰값 검증 후 컨트롤러로 요청 전달

--- src/main/java/coffeemeet/server/common/config/WebConfig.java
+++ src/main/java/coffeemeet/server/common/config/WebConfig.java
@@ -1,11 +1,17 @@
 package coffeemeet.server.common.config;
 
+import coffeemeet.server.common.presentation.interceptor.AuthenticationInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+  private final AuthenticationInterceptor authenticationInterceptor;
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
@@ -13,6 +19,12 @@ public class WebConfig implements WebMvcConfigurer {
         .allowedOrigins("*")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*");
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(authenticationInterceptor)
+        .addPathPatterns("/api/v1/chatting/rooms/**");
   }
 
 }
