d420454034f4cb83ae6399397a2a82da12400701
sangminnim <sangmins930@gmail.com>
2023-11-13 17:25:29 +0900

    refactor: 정책 변경으로 인한 매칭 로직 수정
    Co-authored-by: Sangmin Park <70051888+smart-sangmin@users.noreply.github.com>
    Co-authored-by: 박은지 <qkrdmswl1018@naver.com>

--- src/main/java/coffeemeet/server/matching/presentation/MatchingController.java
+++ src/main/java/coffeemeet/server/matching/presentation/MatchingController.java
@@ -2,13 +2,10 @@ package coffeemeet.server.matching.presentation;
 
 import coffeemeet.server.common.annotation.Login;
 import coffeemeet.server.common.domain.AuthInfo;
-import coffeemeet.server.matching.presentation.dto.AcceptResponseHttp;
 import coffeemeet.server.matching.service.MatchingService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,16 +19,6 @@ public class MatchingController {
   @PostMapping("/start")
   public ResponseEntity<Void> start(@Login AuthInfo authInfo) {
     matchingService.start(authInfo.userId());
-    return ResponseEntity.ok().build();
-  }
-
-  @PostMapping("/decision")
-  public ResponseEntity<Void> acceptDecision(@Valid @RequestBody AcceptResponseHttp.Request request) {
-    matchingService.processMatchDecision(
-        request.userId(),
-        request.isAccepted(),
-        request.waitingRoomId()
-    );
     return ResponseEntity.ok().build();
   }
 
