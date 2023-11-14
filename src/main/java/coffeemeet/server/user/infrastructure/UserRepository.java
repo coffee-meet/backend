26ff030e54fddccea6f0529464b02bb06ccbbb8c
yumyeonghan <mgtmh991013@gmail.com>
2023-11-14 17:41:14 +0900

    feat: 같은 채팅방에 있는 유저 전체 조회 메서드 작성

--- src/main/java/coffeemeet/server/user/infrastructure/UserRepository.java
+++ src/main/java/coffeemeet/server/user/infrastructure/UserRepository.java
@@ -1,8 +1,10 @@
 package coffeemeet.server.user.infrastructure;
 
+import coffeemeet.server.chatting.current.domain.ChattingRoom;
 import coffeemeet.server.user.domain.OAuthInfo;
 import coffeemeet.server.user.domain.User;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -16,5 +18,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findByOauthInfo(OAuthInfo oauthInfo);
 
   boolean existsUserByProfile_Nickname(String nickname);
+
+  List<User> findAllByChattingRoom(ChattingRoom chattingRoom);
 
 }
