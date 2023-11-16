774c55d52da4498c87ff3538c3c581c5b122d5ff
박은지 <qkrdmswl1018@naver.com>
2023-11-17 01:00:50 +0900

    refactor: 신고 DTO 필드 수정

--- src/main/java/coffeemeet/server/report/service/dto/ReportDto.java
+++ src/main/java/coffeemeet/server/report/service/dto/ReportDto.java
@@ -1,28 +1,29 @@
 package coffeemeet.server.report.service.dto;
 
 import coffeemeet.server.report.domain.Report;
+import coffeemeet.server.user.domain.User;
 import java.time.LocalDateTime;
 
 public sealed interface ReportDto permits ReportDto.Response {
 
   record Response(
-      Long reporterId,
-      Long chattingRoomId,
-      Long targetId,
+      String reporterNickname,
+      String targetNickname,
       String reporterEmail,
       String reason,
       String reasonDetail,
+      int reportedCount,
       LocalDateTime createAt
   ) implements ReportDto {
 
-    public static Response of(Report report) {
+    public static Response of(Report report, User reporter, User targetUser) {
       return new Response(
-          report.getReporterId(),
-          report.getChattingRoomId(),
-          report.getTargetId(),
+          reporter.getProfile().getNickname(),
+          targetUser.getProfile().getNickname(),
           report.getReporterEmail().getValue(),
           report.getReason(),
           report.getReasonDetail(),
+          targetUser.getReportInfo().getReportedCount(),
           report.getCreatedAt()
       );
     }
