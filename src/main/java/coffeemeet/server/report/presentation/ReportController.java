9cd30920a32823444ff0f04bda675e9b5fbbefa7
박은지 <qkrdmswl1018@naver.com>
2023-11-17 02:16:02 +0900

    feat: 신고 전체 조회 기능 구현

--- src/main/java/coffeemeet/server/report/presentation/ReportController.java
+++ src/main/java/coffeemeet/server/report/presentation/ReportController.java
@@ -2,10 +2,13 @@ package coffeemeet.server.report.presentation;
 
 import coffeemeet.server.common.annotation.Login;
 import coffeemeet.server.common.domain.AuthInfo;
+import coffeemeet.server.report.presentation.dto.AllReportHTTP;
 import coffeemeet.server.report.presentation.dto.ReportHTTP;
 import coffeemeet.server.report.service.ReportService;
+import coffeemeet.server.report.service.dto.AllReportDto;
 import coffeemeet.server.report.service.dto.ReportDto;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,6 +37,15 @@ public class ReportController {
   public ResponseEntity<ReportHTTP.Response> findReport(@PathVariable Long reportId) {
     ReportDto.Response report = reportService.findReport(reportId);
     return ResponseEntity.ok(ReportHTTP.Response.of(report));
+  }
+
+  @GetMapping
+  public ResponseEntity<List<AllReportHTTP.Response>> findAllReports() {
+    List<AllReportDto.Response> reports = reportService.findAllReports();
+    List<AllReportHTTP.Response> response = reports.stream()
+        .map(AllReportHTTP.Response::of)
+        .toList();
+    return ResponseEntity.ok(response);
   }
 
 }
