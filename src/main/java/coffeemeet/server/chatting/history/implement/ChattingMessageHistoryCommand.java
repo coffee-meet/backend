7018fd3f7c69718728b1a53ec6a2bd9a06b9b409
yumyeonghan <mgtmh991013@gmail.com>
2023-11-16 10:58:32 +0900

    feat: createChattingMessageHistory() 작성

---
+++ src/main/java/coffeemeet/server/chatting/history/implement/ChattingMessageHistoryCommand.java
@@ -0,0 +1,21 @@
+package coffeemeet.server.chatting.history.implement;
+
+import coffeemeet.server.chatting.history.domain.ChattingMessageHistory;
+import coffeemeet.server.chatting.history.infrastructure.ChattingMessageHistoryRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Transactional
+@RequiredArgsConstructor
+public class ChattingMessageHistoryCommand {
+
+  private final ChattingMessageHistoryRepository chattingMessageHistoryRepository;
+
+  public void createChattingMessageHistory(List<ChattingMessageHistory> messages) {
+    chattingMessageHistoryRepository.saveAll(messages);
+  }
+
+}
---
+++ src/main/java/coffeemeet/server/chatting/history/infrastructure/ChattingMessageHistoryRepository.java
@@ -0,0 +1,9 @@
+package coffeemeet.server.chatting.history.infrastructure;
+
+import coffeemeet.server.chatting.history.domain.ChattingMessageHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChattingMessageHistoryRepository extends
+    JpaRepository<ChattingMessageHistory, Long> {
+
+}
