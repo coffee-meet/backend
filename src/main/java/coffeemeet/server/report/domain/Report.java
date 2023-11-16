3686f9c4076c0eb15f4125c823596bd645a5f24e
박은지 <qkrdmswl1018@naver.com>
2023-11-16 23:44:11 +0900

    refactor: 신고 엔티티 검증 로직 수정 및 필드명 변경

--- src/main/java/coffeemeet/server/report/domain/Report.java
+++ src/main/java/coffeemeet/server/report/domain/Report.java
@@ -1,7 +1,16 @@
 package coffeemeet.server.report.domain;
 
+import static coffeemeet.server.report.exception.ReportErrorCode.INVALID_CHATTING_ROOM;
+import static coffeemeet.server.report.exception.ReportErrorCode.INVALID_REASON;
+import static coffeemeet.server.report.exception.ReportErrorCode.INVALID_REASON_DETAIL;
+import static coffeemeet.server.report.exception.ReportErrorCode.INVALID_REPORTER;
+import static coffeemeet.server.report.exception.ReportErrorCode.INVALID_TARGET_USER;
+
 import coffeemeet.server.common.domain.BaseEntity;
+import coffeemeet.server.common.execption.InvalidInputException;
+import coffeemeet.server.user.domain.Email;
 import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,6 +20,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import org.springframework.util.StringUtils;
 
 @Entity
@@ -19,7 +29,12 @@ import org.springframework.util.StringUtils;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Report extends BaseEntity {
 
-  private static final int TITLE_MAX_LENGTH = 20;
+  private static final String INVALID_REPORTER_MESSAGE = "해당 신고자 아이디(%s)는 유효하지 않습니다.";
+  private static final String INVALID_CHATTING_ROOM_MESSAGE = "해당 채팅방 아이디(%s)는 유효하지 않습니다.";
+  private static final String INVALID_TARGET_USER_MESSAGE = "해당 신고 대상 아이디(%s)는 유효하지 않습니다.";
+  private static final String INVALID_REASON_MESSAGE = "해당 신고 사유(%s)는 유효하지 않습니다.";
+  private static final String INVALID_REASON_DETAIL_MESSAGE = "해당 신고 상세 사유(%s)는 유효하지 않습니다.";
+
   private static final int REASON_MAX_LENGTH = 200;
 
   @Id
@@ -30,52 +45,85 @@ public class Report extends BaseEntity {
   private Long reporterId;
 
   @Column(nullable = false)
+  private Long chattingRoomId;
+
+  @Column(nullable = false)
   private Long targetId;
 
-  @Column(nullable = false, length = TITLE_MAX_LENGTH)
-  private String title;
+  @Embedded
+  @Column(nullable = false)
+  private Email reporterEmail;
+
+  @Column(nullable = false)
+  private String reason;
 
   @Column(nullable = false, length = REASON_MAX_LENGTH)
-  private String reason;
+  private String reasonDetail;
 
   @Builder
   private Report(
-      Long reporterId,
-      Long targetId,
-      String title,
-      String reason
+      @NonNull Long reporterId,
+      @NonNull Long chattingRoomId,
+      @NonNull Long targetId,
+      @NonNull Email reporterEmail,
+      @NonNull String reason,
+      @NonNull String reasonDetail
   ) {
     validateReporter(reporterId);
+    validateChattingRoom(chattingRoomId);
     validateTarget(targetId);
-    validateTitle(title);
     validateReason(reason);
+    validateReasonDetails(reasonDetail);
     this.reporterId = reporterId;
+    this.chattingRoomId = chattingRoomId;
     this.targetId = targetId;
-    this.title = title;
+    this.reporterEmail = reporterEmail;
     this.reason = reason;
+    this.reasonDetail = reasonDetail;
   }
 
   private void validateReporter(Long reporterId) {
     if (reporterId == null) {
-      throw new IllegalArgumentException("올바르지 않은 사용자(리포터) 아이디입니다.");
+      throw new InvalidInputException(
+          INVALID_REPORTER,
+          INVALID_REPORTER_MESSAGE
+      );
+    }
+  }
+
+  private void validateChattingRoom(Long chattingRoomId) {
+    if (chattingRoomId == null) {
+      throw new InvalidInputException(
+          INVALID_CHATTING_ROOM,
+          INVALID_CHATTING_ROOM_MESSAGE
+      );
     }
   }
 
   private void validateTarget(Long targetId) {
     if (targetId == null) {
-      throw new IllegalArgumentException("올바르지 않은 사용자(타켓) 아이디입니다.");
-    }
-  }
-
-  private void validateTitle(String title) {
-    if (!StringUtils.hasText(title) || title.length() > TITLE_MAX_LENGTH) {
-      throw new IllegalArgumentException("올바르지 않은 제목입니다.");
+      throw new InvalidInputException(
+          INVALID_TARGET_USER,
+          INVALID_TARGET_USER_MESSAGE
+      );
     }
   }
 
   private void validateReason(String reason) {
     if (!StringUtils.hasText(reason) || reason.length() > REASON_MAX_LENGTH) {
-      throw new IllegalArgumentException("올바르지 않은 사유입니다.");
+      throw new InvalidInputException(
+          INVALID_REASON,
+          String.format(INVALID_REASON_MESSAGE, reason)
+      );
+    }
+  }
+
+  private void validateReasonDetails(String reasonDetails) {
+    if (!StringUtils.hasText(reasonDetails) || reasonDetails.length() > REASON_MAX_LENGTH) {
+      throw new InvalidInputException(
+          INVALID_REASON_DETAIL,
+          String.format(INVALID_REASON_DETAIL_MESSAGE, reasonDetails)
+      );
     }
   }
 
