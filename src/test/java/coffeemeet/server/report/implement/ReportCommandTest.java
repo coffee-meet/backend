890d8d9508573f4d6ce21af6899848d77ce1e334
박은지 <qkrdmswl1018@naver.com>
2023-11-17 03:10:44 +0900

    test: ReportCommand 테스트 코드 작성

---
+++ src/test/java/coffeemeet/server/report/implement/ReportCommandTest.java
@@ -0,0 +1,37 @@
+package coffeemeet.server.report.implement;
+
+import static coffeemeet.server.common.fixture.entity.ReportFixture.report;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.mockito.BDDMockito.willDoNothing;
+
+import coffeemeet.server.report.domain.Report;
+import coffeemeet.server.report.infrastructure.ReportRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ReportCommandTest {
+
+  @InjectMocks
+  private ReportCommand reportCommand;
+
+  @Mock
+  private ReportRepository reportRepository;
+
+  @Test
+  @DisplayName("신고를 생성해 저장할 수 있다.")
+  void saveTest() {
+    // given
+    Report report = report();
+
+    willDoNothing().given(reportRepository).save(report);
+
+    // when, then
+    assertThatCode(() -> reportCommand.save(report)).doesNotThrowAnyException();
+  }
+
+}
