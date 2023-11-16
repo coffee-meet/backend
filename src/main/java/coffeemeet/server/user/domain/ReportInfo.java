0dec62b27ddcdb4ba3aa949b5628aa66f0d2dc82
sangminnim <sangmins930@gmail.com>
2023-11-17 03:39:16 +0900

    refactor: 제재기간 변수명 변경, 신고 횟수 증가 메서드 구현

--- src/main/java/coffeemeet/server/user/domain/ReportInfo.java
+++ src/main/java/coffeemeet/server/user/domain/ReportInfo.java
@@ -1,5 +1,9 @@
 package coffeemeet.server.user.domain;
 
+import static coffeemeet.server.report.exception.ReportErrorCode.EXCEEDED_MAX_REPORT_COUNT;
+
+import coffeemeet.server.common.execption.LimitExceededException;
+import coffeemeet.server.report.domain.ReportPenalty;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import java.time.LocalDateTime;
@@ -10,15 +14,29 @@ import lombok.Getter;
 public class ReportInfo {
 
   private static final int REPORT_MIN_COUNT = 0;
+  private static final int REPORT_MAX_COUNT = 5;
 
   @Column(nullable = false)
   private int reportedCount;
 
-  private LocalDateTime sanctionPeriod;
+  private LocalDateTime penaltyExpiration;
 
   public ReportInfo() {
     this.reportedCount = REPORT_MIN_COUNT;
-    this.sanctionPeriod = null;
+    this.penaltyExpiration = null;
+  }
+
+  public void increment() {
+    if (reportedCount >= REPORT_MAX_COUNT) {
+      throw new LimitExceededException(EXCEEDED_MAX_REPORT_COUNT,
+          String.format("최대 신고 누적 횟수를 초과합니다. 신고 누적 횟수(%s)", reportedCount));
+    }
+    this.reportedCount++;
+    int sanctionPeriodByReportCount = ReportPenalty.getSanctionPeriodByReportCount(reportedCount);
+    if (sanctionPeriodByReportCount == -1) {
+      penaltyExpiration = LocalDateTime.MAX;
+    }
+    penaltyExpiration = LocalDateTime.now().plusDays(sanctionPeriodByReportCount);
   }
 
 }
