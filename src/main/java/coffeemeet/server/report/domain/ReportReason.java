0c863e9bde9ff902bab5067a1f6ecade34c9eae2
박은지 <qkrdmswl1018@naver.com>
2023-11-18 02:25:22 +0900

    refactor: 신고 사유 enum 클래스 값 변경

--- src/main/java/coffeemeet/server/report/domain/ReportReason.java
+++ src/main/java/coffeemeet/server/report/domain/ReportReason.java
@@ -3,31 +3,29 @@ package coffeemeet.server.report.domain;
 import static coffeemeet.server.report.exception.ReportErrorCode.INVALID_REASON;
 
 import coffeemeet.server.common.execption.InvalidInputException;
-import java.util.Arrays;
 
 public enum ReportReason {
-  HATE_SPEECH("혐오 발언"),
-  HARASSMENT_OR_STALKING("괴롭힘 또는 스토킹"),
-  SEXUAL_HARASSMENT("성적인 괴롭힘"),
-  SPAM_AND_ADVERTISING("스팸 및 광고"),
-  INAPPROPRIATE_CONTENT("부적절한 콘텐츠"),
-  INACTIVE("잠수");
+  혐오_발언,
+  괴롭힘_또는_스토킹,
+  성적인_괴롭힘,
+  스팸_및_광고,
+  부적절한_콘텐츠,
+  잠수;
 
   private static final String INVALID_REASON_MESSAGE = "해당 신고 사유(%s)는 유효하지 않습니다.";
-  private final String reason;
-
-  ReportReason(String reason) {
-    this.reason = reason;
-  }
 
   public static ReportReason getReason(String reason) {
-    return Arrays.stream(ReportReason.values())
-        .filter(reportReason -> reportReason.reason.equals(reason))
-        .findAny()
-        .orElseThrow(() -> new InvalidInputException(
-            INVALID_REASON,
-            String.format(INVALID_REASON_MESSAGE, reason))
-        );
+    return switch (reason) {
+      case "혐오 발언", "혐오_발언" -> 혐오_발언;
+      case "괴롭힘 또는 스토킹", "괴롭힘_또는_스토킹" -> 괴롭힘_또는_스토킹;
+      case "성적인 괴롭힘", "성적인_괴롭힘" -> 성적인_괴롭힘;
+      case "스팸 및 광고", "스팸_및_광고" -> 스팸_및_광고;
+      case "부적절한 콘텐츠", "부적절한_콘텐츠" -> 부적절한_콘텐츠;
+      case "잠수" -> 잠수;
+      default -> throw new InvalidInputException(
+          INVALID_REASON,
+          String.format(INVALID_REASON_MESSAGE, reason));
+    };
   }
 
 }
