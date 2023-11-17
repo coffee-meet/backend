55ef66649d96d610d1fd88372928e299680e3188
박은지 <qkrdmswl1018@naver.com>
2023-11-17 15:49:24 +0900

    refactor: 신고 테스트 Fixture 수정

--- src/test/java/coffeemeet/server/common/fixture/entity/ReportFixture.java
+++ src/test/java/coffeemeet/server/common/fixture/entity/ReportFixture.java
@@ -3,14 +3,19 @@ package coffeemeet.server.common.fixture.entity;
 import static org.instancio.Select.field;
 
 import coffeemeet.server.report.domain.Report;
-import coffeemeet.server.user.domain.Email;
 import org.instancio.Instancio;
 
 public class ReportFixture {
 
   public static Report report() {
     return Instancio.of(Report.class)
-        .set(field(Report::getReporterEmail), new Email("test123@gmail.com"))
+        .generate(field(Report::getReasonDetail), gen -> gen.string().maxLength(200)).create();
+  }
+
+  public static Report report(long targetId, long chattingRoomId) {
+    return Instancio.of(Report.class)
+        .set(field(Report::getTargetId), targetId)
+        .set(field(Report::getChattingRoomId), chattingRoomId)
         .generate(field(Report::getReasonDetail), gen -> gen.string().maxLength(200)).create();
   }
 
