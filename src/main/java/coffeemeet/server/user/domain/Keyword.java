bdf1fbc54b8b3d2abea9ef47d9b705c6b1420d85
yumyeonghan <mgtmh991013@gmail.com>
2023-11-17 18:40:34 +0900

    feat: 관심사 추가

--- src/main/java/coffeemeet/server/user/domain/Keyword.java
+++ src/main/java/coffeemeet/server/user/domain/Keyword.java
@@ -1,8 +1,18 @@
 package coffeemeet.server.user.domain;
 
+// TODO: 11/17/23 임시로 한글 
 public enum Keyword {
-  COOK,
-  GAME,
-  EXERCISE,
-
+  요리,
+  맛집,
+  음악,
+  연애,
+  패션,
+  여행,
+  운동,
+  게임,
+  제태크,
+  자격증,
+  외국어,
+  전자기기,
+  반려동물
 }
