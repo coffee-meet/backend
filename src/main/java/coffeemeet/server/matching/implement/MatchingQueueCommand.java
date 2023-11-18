4ec9fb5084120ea2b851aa6bf86bd3db69e4849f
sangminnim <sangmins930@gmail.com>
2023-11-18 17:00:08 +0900

    refactor: 레디스 예외 아규먼트 위치 변경

--- src/main/java/coffeemeet/server/matching/implement/MatchingQueueCommand.java
+++ src/main/java/coffeemeet/server/matching/implement/MatchingQueueCommand.java
@@ -18,7 +18,7 @@ public class MatchingQueueCommand {
     ZSetOperations<String, Long> zSetOperations = redisTemplate.opsForZSet();
     Boolean result = zSetOperations.add(companyName, userId, System.currentTimeMillis());
     if (result == null) {
-      throw new RedisException("Redis가 Pipeline 상태이거나 Transaction 상태입니다.", INTERNAL_SERVER_ERROR);
+      throw new RedisException(INTERNAL_SERVER_ERROR, "Redis가 Pipeline 상태이거나 Transaction 상태입니다.");
     }
   }
 
