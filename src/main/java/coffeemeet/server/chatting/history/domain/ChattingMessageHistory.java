a24ca2c5bc71c8c5546d3d0b3170b8eb54a371a3
yumyeonghan <mgtmh991013@gmail.com>
2023-11-20 01:40:12 +0900

    refactor: createdAt 추가
    - 생성자로 직접 설정

--- src/main/java/coffeemeet/server/chatting/history/domain/ChattingMessageHistory.java
+++ src/main/java/coffeemeet/server/chatting/history/domain/ChattingMessageHistory.java
@@ -2,7 +2,6 @@ package coffeemeet.server.chatting.history.domain;
 
 import static coffeemeet.server.chatting.exception.ChattingErrorCode.INVALID_MESSAGE;
 
-import coffeemeet.server.common.domain.BaseEntity;
 import coffeemeet.server.common.execption.InvalidInputException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -12,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,7 +22,7 @@ import org.springframework.util.StringUtils;
 @Getter
 @Table(name = "chatting_message_histories")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ChattingMessageHistory extends BaseEntity {
+public class ChattingMessageHistory {
 
   private static final String INVALID_CHATTING_MESSAGE = "입력된 (%s)는 올바르지 않은 메시지입니다.";
 
@@ -33,15 +33,19 @@ public class ChattingMessageHistory extends BaseEntity {
   @Column(nullable = false)
   private String message;
 
+  @Column(nullable = false)
+  private LocalDateTime createdAt;
+
   @ManyToOne
   @JoinColumn(name = "chatting_room_history_id", nullable = false)
   private ChattingRoomHistory chattingRoomHistory;
 
   public ChattingMessageHistory(@NonNull String message,
-      @NonNull ChattingRoomHistory chattingRoomHistory) {
+      @NonNull ChattingRoomHistory chattingRoomHistory, @NonNull LocalDateTime createdAt) {
     validateMessage(message);
     this.message = message;
     this.chattingRoomHistory = chattingRoomHistory;
+    this.createdAt = createdAt;
   }
 
   private void validateMessage(String message) {
