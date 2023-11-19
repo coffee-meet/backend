0fcb1f4c06df0530e5554658680f870357e1defe
yumyeonghan <mgtmh991013@gmail.com>
2023-11-20 02:26:36 +0900

    refactor: removeChattingRoom 리팩토링 및 테스트 작성

--- src/main/java/coffeemeet/server/chatting/current/implement/ChattingRoomCommand.java
+++ src/main/java/coffeemeet/server/chatting/current/implement/ChattingRoomCommand.java
@@ -2,6 +2,9 @@ package coffeemeet.server.chatting.current.implement;
 
 import coffeemeet.server.chatting.current.domain.ChattingRoom;
 import coffeemeet.server.chatting.current.infrastructure.ChattingRoomRepository;
+import coffeemeet.server.user.domain.User;
+import coffeemeet.server.user.implement.UserQuery;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,13 +15,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class ChattingRoomCommand {
 
   private final ChattingRoomRepository chattingRoomRepository;
+  private final ChattingMessageCommand chattingMessageCommand;
+  private final UserQuery userQuery;
 
   public ChattingRoom createChattingRoom() {
     return chattingRoomRepository.save(new ChattingRoom());
   }
 
-  public void removeChattingRoom(Long roomId) {
-    chattingRoomRepository.deleteById(roomId);
+  public void removeChattingRoom(ChattingRoom chattingRoom) {
+    chattingMessageCommand.deleteAllChattingMessagesBy(chattingRoom);
+    List<User> users = userQuery.getUsersByRoom(chattingRoom);
+    users.forEach(User::deleteChattingRoom);
+    chattingRoomRepository.deleteById(chattingRoom.getId());
   }
 
 }
--- src/test/java/coffeemeet/server/chatting/current/implement/ChattingRoomCommandTest.java
+++ src/test/java/coffeemeet/server/chatting/current/implement/ChattingRoomCommandTest.java
@@ -1,11 +1,19 @@
 package coffeemeet.server.chatting.current.implement;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.times;
 
+import coffeemeet.server.chatting.current.domain.ChattingMessage;
 import coffeemeet.server.chatting.current.domain.ChattingRoom;
 import coffeemeet.server.chatting.current.infrastructure.ChattingRoomRepository;
+import coffeemeet.server.common.fixture.entity.ChattingFixture;
+import coffeemeet.server.common.fixture.entity.UserFixture;
+import coffeemeet.server.user.domain.User;
+import coffeemeet.server.user.implement.UserQuery;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -22,6 +30,12 @@ class ChattingRoomCommandTest {
   @Mock
   private ChattingRoomRepository chattingRoomRepository;
 
+  @Mock
+  private ChattingMessageCommand chattingMessageCommand;
+
+  @Mock
+  private UserQuery userQuery;
+
   @DisplayName("채팅방을 생성할 수 있다.")
   @Test
   void saveChattingRoomTest() {
@@ -36,13 +50,17 @@ class ChattingRoomCommandTest {
   @Test
   void removeChattingRoomTest() {
     // given
-    Long roomId = 1L;
+    ChattingRoom chattingRoom = ChattingFixture.chattingRoom();
+    List<ChattingMessage> chattingMessages = ChattingFixture.chattingMessages(10);
+    List<User> users = UserFixture.users();
+    given(userQuery.getUsersByRoom(chattingRoom)).willReturn(users);
 
     // when
-    chattingRoomCommand.removeChattingRoom(roomId);
+    chattingRoomCommand.removeChattingRoom(chattingRoom);
 
     // then
-    then(chattingRoomRepository).should(only()).deleteById(roomId);
+    then(chattingMessageCommand).should(times(1)).deleteAllChattingMessagesBy(chattingRoom);
+    then(chattingRoomRepository).should(only()).deleteById(chattingRoom.getId());
   }
 
 }
