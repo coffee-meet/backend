133c573e569bc366c6768e5ec8daed6031e9a1bb
yumyeonghan <mgtmh991013@gmail.com>
2023-11-20 02:28:18 +0900

    refactor: createChattingRoomHistory 리팩토링 및 테스트 작성

--- src/main/java/coffeemeet/server/chatting/history/implement/ChattingRoomHistoryCommand.java
+++ src/main/java/coffeemeet/server/chatting/history/implement/ChattingRoomHistoryCommand.java
@@ -1,5 +1,6 @@
 package coffeemeet.server.chatting.history.implement;
 
+import coffeemeet.server.chatting.current.domain.ChattingRoom;
 import coffeemeet.server.chatting.history.domain.ChattingRoomHistory;
 import coffeemeet.server.chatting.history.infrastructure.ChattingRoomHistoryRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,8 +14,8 @@ public class ChattingRoomHistoryCommand {
 
   private final ChattingRoomHistoryRepository chattingRoomHistoryRepository;
 
-  public ChattingRoomHistory createChattingRoomHistory() {
-    return chattingRoomHistoryRepository.save(new ChattingRoomHistory());
+  public ChattingRoomHistory createChattingRoomHistory(ChattingRoom chattingRoom) {
+    return chattingRoomHistoryRepository.save(new ChattingRoomHistory(chattingRoom.getName()));
   }
 
 }
