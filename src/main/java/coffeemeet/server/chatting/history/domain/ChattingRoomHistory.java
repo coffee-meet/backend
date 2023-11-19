048851781f6f8b55054e4303b71e8a9048afa12b
yumyeonghan <mgtmh991013@gmail.com>
2023-11-20 02:27:45 +0900

    feat: 이름 컬럼 추가

--- src/main/java/coffeemeet/server/chatting/history/domain/ChattingRoomHistory.java
+++ src/main/java/coffeemeet/server/chatting/history/domain/ChattingRoomHistory.java
@@ -1,22 +1,32 @@
 package coffeemeet.server.chatting.history.domain;
 
 import coffeemeet.server.common.domain.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Entity
 @Table(name = "chatting_room_histories")
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ChattingRoomHistory extends BaseEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  public ChattingRoomHistory(@NonNull String name) {
+    this.name = name;
+  }
 
 }
