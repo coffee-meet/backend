644cbd0c7375530f6c285f4aae2b28e5f60d0ba1
yumyeonghan <mgtmh991013@gmail.com>
2023-11-20 23:56:55 +0900

    feat: @GeneratedValue 추가

--- src/main/java/coffeemeet/server/chatting/history/domain/UserChattingHistory.java
+++ src/main/java/coffeemeet/server/chatting/history/domain/UserChattingHistory.java
@@ -4,6 +4,8 @@ import coffeemeet.server.common.domain.BaseEntity;
 import coffeemeet.server.user.domain.User;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -20,6 +22,7 @@ import lombok.NonNull;
 public class UserChattingHistory extends BaseEntity {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   @ManyToOne(fetch = FetchType.LAZY)
