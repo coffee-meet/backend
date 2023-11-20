4d6a24b22018a38f91e97087eea580c4bf761cbf
yumyeonghan <mgtmh991013@gmail.com>
2023-11-20 16:06:29 +0900

    feat: 채팅방 내역 및 채팅 메세지 내역 Dto 생성

---
+++ src/main/java/coffeemeet/server/chatting/history/presentation/dto/ChattingRoomHistoriesHTTP.java
@@ -0,0 +1,38 @@
+package coffeemeet.server.chatting.history.presentation.dto;
+
+import coffeemeet.server.chatting.history.service.dto.ChattingRoomHistoryDto;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public sealed interface ChattingRoomHistoriesHTTP permits ChattingRoomHistoriesHTTP.Response {
+
+  record ChatRoomHistory(
+      Long roomId,
+      String roomName,
+      List<String> users,
+      LocalDateTime createdAt
+  ) {
+
+    public static ChatRoomHistory from(ChattingRoomHistoryDto.Response response) {
+      return new ChatRoomHistory(
+          response.roomId(),
+          response.roomName(),
+          response.users(),
+          response.createdAt()
+      );
+    }
+
+  }
+
+  record Response(List<ChatRoomHistory> chatRoomHistories) implements ChattingRoomHistoriesHTTP {
+
+    public static Response from(List<ChattingRoomHistoryDto.Response> responses) {
+      List<ChatRoomHistory> roomHistories = responses.stream()
+          .map(ChatRoomHistory::from)
+          .toList();
+      return new Response(roomHistories);
+    }
+
+  }
+
+}
