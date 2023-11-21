ba852a0ef2bd2eaf7a21217623a333a39f8383b2
yumyeonghan <mgtmh991013@gmail.com>
2023-11-21 16:54:36 +0900

    refactor: dto 수정

--- src/main/java/coffeemeet/server/user/presentation/dto/SignupHTTP.java
+++ src/main/java/coffeemeet/server/user/presentation/dto/SignupHTTP.java
@@ -3,24 +3,22 @@ package coffeemeet.server.user.presentation.dto;
 import static lombok.AccessLevel.PRIVATE;
 
 import coffeemeet.server.user.domain.Keyword;
-import coffeemeet.server.user.domain.OAuthProvider;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @NoArgsConstructor(access = PRIVATE)
 public final class SignupHTTP {
 
   public record Request(
+      @NonNull
+      Long userId,
       @NotBlank
       String nickname,
       @NotNull
-      List<Keyword> keywords,
-      @NotBlank
-      String authCode,
-      @NotNull
-      OAuthProvider oAuthProvider
+      List<Keyword> keywords
   ) {
 
   }
