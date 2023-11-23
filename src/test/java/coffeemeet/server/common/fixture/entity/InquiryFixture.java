e2a21db361f7541d2510a3f314aaa71c87f6469a
yumyeonghan <mgtmh991013@gmail.com>
2023-11-24 00:15:52 +0900

    test: 테스트 문의 픽스처 추가 및 설정 파일 수정

--- src/test/java/coffeemeet/server/common/fixture/entity/InquiryFixture.java
+++ src/test/java/coffeemeet/server/common/fixture/entity/InquiryFixture.java
@@ -4,12 +4,14 @@ import static org.instancio.Select.field;
 
 import coffeemeet.server.inquiry.domain.Inquiry;
 import coffeemeet.server.inquiry.presentation.dto.InquiryHTTP;
+import java.util.List;
 import org.instancio.Instancio;
 
 public class InquiryFixture {
 
   public static Inquiry inquiry() {
     return Instancio.of(Inquiry.class)
+        .set(field(Inquiry::isCheck), false)
         .generate(field(Inquiry::getTitle), gen -> gen.string().maxLength(20))
         .generate(field(Inquiry::getContent), gen -> gen.string().maxLength(200)).create();
   }
@@ -18,6 +20,14 @@ public class InquiryFixture {
     return Instancio.of(InquiryHTTP.Request.class)
         .generate(field(InquiryHTTP.Request::title), gen -> gen.string().maxLength(20))
         .generate(field(InquiryHTTP.Request::content), gen -> gen.string().maxLength(200)).create();
+  }
+
+  public static List<Inquiry> inquiries(int size) {
+    return Instancio.ofList(Inquiry.class).size(size)
+        .set(field(Inquiry::isCheck), false)
+        .generate(field(Inquiry::getId), gen -> gen.longSeq().start(1L))
+        .generate(field(Inquiry::getTitle), gen -> gen.string().maxLength(20))
+        .generate(field(Inquiry::getContent), gen -> gen.string().maxLength(200)).create();
   }
 
 }
