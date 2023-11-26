90a20c0581be75e74542122cca59c345f67a7d5b
sangminnim <sangmins930@gmail.com>
2023-11-27 05:26:15 +0900

    feat: 인증되지 않은 페이지 불러오기 구현

--- src/main/java/coffeemeet/server/certification/infrastructure/CertificationRepository.java
+++ src/main/java/coffeemeet/server/certification/infrastructure/CertificationRepository.java
@@ -3,6 +3,8 @@ package coffeemeet.server.certification.infrastructure;
 import coffeemeet.server.certification.domain.Certification;
 import coffeemeet.server.certification.domain.CompanyEmail;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CertificationRepository extends JpaRepository<Certification, Long> {
@@ -10,5 +12,7 @@ public interface CertificationRepository extends JpaRepository<Certification, Lo
   Optional<Certification> findByUserId(Long userId);
 
   boolean existsByCompanyEmail(CompanyEmail companyEmail);
+
+  Page<Certification> findByIsCertificatedIsFalse(Pageable pageable);
 
 }
