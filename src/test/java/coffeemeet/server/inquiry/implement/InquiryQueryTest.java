7edbe4ed0cafe77129e490cbf888c7d149fec381
yumyeonghan <mgtmh991013@gmail.com>
2023-11-26 12:57:55 +0900

    test: getInquiryBy() 테스트 작성

--- src/test/java/coffeemeet/server/inquiry/implement/InquiryQueryTest.java
+++ src/test/java/coffeemeet/server/inquiry/implement/InquiryQueryTest.java
@@ -1,12 +1,15 @@
 package coffeemeet.server.inquiry.implement;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 
 import coffeemeet.server.common.fixture.entity.InquiryFixture;
 import coffeemeet.server.inquiry.domain.Inquiry;
 import coffeemeet.server.inquiry.infrastructure.InquiryQueryRepository;
+import coffeemeet.server.inquiry.infrastructure.InquiryRepository;
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,6 +26,9 @@ class InquiryQueryTest {
   @Mock
   private InquiryQueryRepository inquiryQueryRepository;
 
+  @Mock
+  private InquiryRepository inquiryRepository;
+
   @DisplayName("문의 내역을 가져올 수 있다.")
   @Test
   void getInquiriesByTest() {
@@ -38,6 +44,20 @@ class InquiryQueryTest {
 
     // then
     assertThat(response).isEqualTo(inquiries);
+  }
+
+  @DisplayName("문의 아이디로 문의를 가져올 수 있다.")
+  @Test
+  void getInquiryByInquiryIdTest() {
+    // given
+    Inquiry inquiry = InquiryFixture.inquiry();
+    given(inquiryRepository.findById(anyLong())).willReturn(Optional.of(inquiry));
+
+    // when
+    Inquiry foundInquiry = inquiryQuery.getInquiryBy(inquiry.getInquirerId());
+
+    // then
+    assertThat(foundInquiry).isEqualTo(inquiry);
   }
 
 }
