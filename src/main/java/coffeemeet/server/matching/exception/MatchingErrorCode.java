7cde5e62ebdbdcfb5c148dd37de984752f8e6c59
sangminnim <sangmins930@gmail.com>
2023-12-02 18:42:04 +0900

    refactor: typo

--- src/main/java/coffeemeet/server/matching/exception/MatchingErrorCode.java
+++ src/main/java/coffeemeet/server/matching/exception/MatchingErrorCode.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum MatchingErrorCode implements ErrorCode {
-  NOT_CERTIFICATED_USER("U003", "인증되지 않은 사용자입니다."),
+  NOT_CERTIFICATED_USER("M003", "인증되지 않은 사용자입니다."),
   ;
 
   private final String errorCode;
