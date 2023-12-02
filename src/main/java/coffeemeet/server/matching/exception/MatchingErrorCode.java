b0879dcb32ac69a997fa36f892866110f350352e
sangminnim <sangmins930@gmail.com>
2023-12-02 17:04:31 +0900

    feat: 매칭 에러코드 정의

---
+++ src/main/java/coffeemeet/server/matching/exception/MatchingErrorCode.java
@@ -0,0 +1,27 @@
+package coffeemeet.server.matching.exception;
+
+
+import coffeemeet.server.common.execption.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum MatchingErrorCode implements ErrorCode {
+  NOT_CERTIFICATED_USER("U003", "인증되지 않은 사용자입니다."),
+  ;
+
+  private final String errorCode;
+  private final String message;
+
+  @Override
+  public String code() {
+    return errorCode;
+  }
+
+  @Override
+  public String message() {
+    return message;
+  }
+
+}
