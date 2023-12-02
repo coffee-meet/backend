c4346f386991dfba2a83f5f5b70e30ca4d951471
박은지 <qkrdmswl1018@naver.com>
2023-12-02 21:27:48 +0900

    test: ChattingRoomHistoryQuery 테스트 코드 작성

---
+++ src/test/java/coffeemeet/server/chatting/history/implement/ChattingRoomHistoryQueryTest.java
@@ -0,0 +1,44 @@
+package coffeemeet.server.chatting.history.implement;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+
+import coffeemeet.server.chatting.history.domain.ChattingRoomHistory;
+import coffeemeet.server.chatting.history.infrastructure.ChattingRoomHistoryRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ChattingRoomHistoryQueryTest {
+
+  @InjectMocks
+  private ChattingRoomHistoryQuery chattingRoomHistoryQuery;
+
+  @Mock
+  private ChattingRoomHistoryRepository chattingRoomHistoryRepository;
+
+  @Test
+  @DisplayName("채팅방 기록 아이디로 해당 채팅방을 가져올 수 있다.")
+  void getChattingRoomHistoryBy() {
+    // given
+    Long chattingRoomHistoryId = 1L;
+    ChattingRoomHistory chattingRoomHistory = new ChattingRoomHistory(1L, "멋쟁이토마토");
+
+    given(chattingRoomHistoryRepository.findById(anyLong())).willReturn(
+        Optional.of(chattingRoomHistory));
+
+    // when
+    ChattingRoomHistory foundChattingRoomHistory = chattingRoomHistoryQuery.getChattingRoomHistoryBy(
+        chattingRoomHistoryId);
+
+    // then
+    assertThat(foundChattingRoomHistory.getId()).isEqualTo(chattingRoomHistoryId);
+  }
+
+}
