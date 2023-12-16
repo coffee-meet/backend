095baae58fde74eae879235260df4db978540912
sangminnim <sangmins930@gmail.com>
2023-12-17 02:31:25 +0900

    refactor: 에러코드 중복 제거

--- src/main/java/coffeemeet/server/common/execption/InvalidAuthException.java
+++ src/main/java/coffeemeet/server/common/execption/InvalidAuthException.java
@@ -1,15 +1,9 @@
 package coffeemeet.server.common.execption;
 
-import lombok.Getter;
-
-@Getter
 public class InvalidAuthException extends CoffeeMeetException {
 
-  private final ErrorCode errorCode;
-
   public InvalidAuthException(ErrorCode errorCode, String message) {
-    super(message);
-    this.errorCode = errorCode;
+    super(errorCode, message);
   }
 
 }
--- src/main/java/coffeemeet/server/common/execption/NotificationFailException.java
+++ src/main/java/coffeemeet/server/common/execption/NotificationFailException.java
@@ -1,15 +1,9 @@
 package coffeemeet.server.common.execption;
 
-import lombok.Getter;
-
-@Getter
 public class NotificationFailException extends CoffeeMeetException {
 
-  private final ErrorCode errorCode;
-
   public NotificationFailException(ErrorCode errorCode, String message) {
-    super(message);
-    this.errorCode = errorCode;
+    super(errorCode, message);
   }
 
 }
