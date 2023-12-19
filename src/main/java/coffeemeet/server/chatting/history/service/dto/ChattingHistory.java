ad129c49152608af8e2d91b0d80f057a0bf34d7b
yumyeonghan <mgtmh991013@gmail.com>
2023-12-19 23:25:27 +0900

    refactor: 네이밍 변경

--- src/main/java/coffeemeet/server/chatting/history/service/dto/ChattingHistory.java
+++ src/main/java/coffeemeet/server/chatting/history/service/dto/ChattingHistory.java
@@ -1,11 +1,14 @@
 package coffeemeet.server.chatting.history.service.dto;
 
-import coffeemeet.server.chatting.history.domain.ChattingMessageHistory;
 import coffeemeet.server.user.domain.User;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 
+<<<<<<<< HEAD:src/main/java/coffeemeet/server/chatting/history/service/dto/ChattingHistory.java
 public record ChattingHistory(
+========
+public record ChattingMessageHistory(
+>>>>>>>> e82e9257 (refactor: 네이밍 변경):src/main/java/coffeemeet/server/chatting/history/service/dto/ChattingMessageHistory.java
     Long userId,
     Long messageId,
     String nickname,
@@ -15,9 +18,15 @@ public record ChattingHistory(
     LocalDateTime createdAt
 ) {
 
+<<<<<<<< HEAD:src/main/java/coffeemeet/server/chatting/history/service/dto/ChattingHistory.java
   public static ChattingHistory of(User user,
       ChattingMessageHistory chattingMessageHistory) {
     return new ChattingHistory(
+========
+  public static ChattingMessageHistory of(User user,
+      coffeemeet.server.chatting.history.domain.ChattingMessageHistory chattingMessageHistory) {
+    return new ChattingMessageHistory(
+>>>>>>>> e82e9257 (refactor: 네이밍 변경):src/main/java/coffeemeet/server/chatting/history/service/dto/ChattingMessageHistory.java
         user.getId(),
         chattingMessageHistory.getId(),
         user.getProfile().getNickname(),
