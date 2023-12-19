11a6eb4453da60c13ed08a8c27264845c6ce6a2f
sangminnim <sangmins930@gmail.com>
2023-12-20 08:56:51 +0900

    test: 테스트 구현 및 메인 코드 변경에 따른 수정

--- src/test/java/coffeemeet/server/certification/domain/repository/CertificationRepositoryTest.java
+++ src/test/java/coffeemeet/server/certification/domain/repository/CertificationRepositoryTest.java
@@ -1,5 +1,10 @@
 package coffeemeet.server.certification.domain.repository;
 
+import static coffeemeet.server.common.fixture.entity.CertificationFixture.certification;
+import static coffeemeet.server.common.fixture.entity.CertificationFixture.certificationPageable;
+import static coffeemeet.server.common.fixture.entity.UserFixture.user;
+import static coffeemeet.server.common.fixture.entity.UserFixture.users;
+import static coffeemeet.server.common.fixture.entity.UserFixture.usersWithNullId;
 import static coffeemeet.server.common.fixture.CertificationFixture.certification;
 import static coffeemeet.server.common.fixture.UserFixture.user;
 import static coffeemeet.server.common.fixture.CertificationFixture.certificationPageable;
