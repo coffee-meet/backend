0f6f7276f607002377418de59a4ad3be2de80199
sangminnim <sangmins930@gmail.com>
2024-01-06 03:03:50 +0900

    refactor: batch 삭제로 수정

--- src/main/java/coffeemeet/server/chatting/current/implement/ChattingMessageCommand.java
+++ src/main/java/coffeemeet/server/chatting/current/implement/ChattingMessageCommand.java
@@ -2,8 +2,7 @@ package coffeemeet.server.chatting.current.implement;
 
 import coffeemeet.server.chatting.current.domain.ChattingMessage;
 import coffeemeet.server.chatting.current.domain.ChattingRoom;
-import coffeemeet.server.chatting.current.infrastructure.ChattingMessageQueryRepository;
-import coffeemeet.server.chatting.current.infrastructure.ChattingMessageRepository;
+import coffeemeet.server.chatting.current.domain.repository.ChattingMessageRepository;
 import coffeemeet.server.user.domain.User;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -16,17 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class ChattingMessageCommand {
 
   private final ChattingMessageRepository chattingMessageRepository;
-  private final ChattingMessageQueryRepository chattingMessageQueryRepository;
 
   public ChattingMessage createChattingMessage(String content, ChattingRoom chattingRoom,
       User user) {
     return chattingMessageRepository.save(new ChattingMessage(content, chattingRoom, user));
   }
 
-  public void deleteAllChattingMessagesBy(ChattingRoom chattingRoom) {
-    List<ChattingMessage> messages = chattingMessageQueryRepository.findAllChattingMessagesByChattingRoom(
-        chattingRoom);
-    chattingMessageRepository.deleteAll(messages);
+  public void deleteChattingMessages(List<ChattingMessage> messages) {
+    chattingMessageRepository.deleteAllInBatch(messages);
   }
 
 }
