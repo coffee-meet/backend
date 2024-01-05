e38614204cefa6df16cda38ae3679566685012de
sangminnim <sangmins930@gmail.com>
2024-01-06 03:12:50 +0900

    refactor: 구현레이어에서 class 사용하여 가독성 있게 변경

--- src/main/java/coffeemeet/server/chatting/current/service/ChattingRoomService.java
+++ src/main/java/coffeemeet/server/chatting/current/service/ChattingRoomService.java
@@ -3,18 +3,15 @@ package coffeemeet.server.chatting.current.service;
 import coffeemeet.server.chatting.current.domain.ChattingMessage;
 import coffeemeet.server.chatting.current.domain.ChattingRoom;
 import coffeemeet.server.chatting.current.implement.ChattingMessageQuery;
+import coffeemeet.server.chatting.current.implement.ChattingMigrationProcessor;
 import coffeemeet.server.chatting.current.implement.ChattingRoomCommand;
+import coffeemeet.server.chatting.current.implement.ChattingRoomNotificationSender;
 import coffeemeet.server.chatting.current.implement.ChattingRoomQuery;
+import coffeemeet.server.chatting.current.implement.ChattingRoomUserValidator;
 import coffeemeet.server.chatting.current.service.dto.Chatting;
 import coffeemeet.server.chatting.current.service.dto.ChattingListDto;
 import coffeemeet.server.chatting.current.service.dto.ChattingRoomStatusDto;
-import coffeemeet.server.chatting.history.domain.ChattingMessageHistory;
 import coffeemeet.server.chatting.history.domain.ChattingRoomHistory;
-import coffeemeet.server.chatting.history.domain.UserChattingHistory;
-import coffeemeet.server.chatting.history.implement.ChattingMessageHistoryCommand;
-import coffeemeet.server.chatting.history.implement.ChattingRoomHistoryCommand;
-import coffeemeet.server.chatting.history.implement.UserChattingHistoryCommand;
-import coffeemeet.server.common.infrastructure.FCMNotificationSender;
 import coffeemeet.server.user.domain.NotificationInfo;
 import coffeemeet.server.user.domain.User;
 import coffeemeet.server.user.implement.UserQuery;
@@ -29,67 +26,55 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ChattingRoomService {
 
-  private static final String CHATTING_END_MESSAGE = "채팅이 종료되었습니다!";
-
   private final ChattingRoomCommand chattingRoomCommand;
   private final ChattingRoomQuery chattingRoomQuery;
   private final ChattingMessageQuery chattingMessageQuery;
-  private final ChattingRoomHistoryCommand chattingRoomHistoryCommand;
-  private final ChattingMessageHistoryCommand chattingMessageHistoryCommand;
-  private final UserChattingHistoryCommand userChattingHistoryCommand;
+  private final ChattingMigrationProcessor chattingMigrationProcessor;
+  private final ChattingRoomNotificationSender chattingRoomNotificationSender;
   private final UserQuery userQuery;
-  private final FCMNotificationSender fcmNotificationSender;
+  private final ChattingRoomUserValidator chattingRoomUserValidator;
 
   public Long createChattingRoom() {
     return chattingRoomCommand.createChattingRoom().getId();
   }
 
   @Transactional
-  public ChattingListDto searchMessages(Long roomId, Long firstMessageId, int pageSize) {
+  public ChattingListDto searchMessages(Long requestedUserId, Long roomId, Long firstMessageId,
+      int pageSize) {
     ChattingRoom chattingRoom = chattingRoomQuery.getChattingRoomById(roomId);
-    List<ChattingMessage> chattingMessages = chattingMessageQuery.findMessages(chattingRoom,
-        firstMessageId,
-        pageSize);
+    List<User> chattingRoomUsers = userQuery.getUsersByRoom(chattingRoom);
+    chattingRoomUserValidator.validateUserInChattingRoom(requestedUserId, chattingRoomUsers);
+
+    List<ChattingMessage> chattingMessages = chattingMessageQuery.getChattingMessagesLessThanMessageId(
+        chattingRoom, firstMessageId, pageSize);
     boolean hasNext = chattingMessages.size() >= pageSize;
+
     List<Chatting> chattingList = chattingMessages.stream()
         .map(message -> Chatting.of(message.getUser(), message)).toList();
     return ChattingListDto.of(chattingList, hasNext);
   }
 
   @Transactional
-  public void deleteChattingRoom(Long roomId) {
+  public void exitChattingRoom(Long requestUserId, Long roomId, Long firstMessageId) {
     ChattingRoom chattingRoom = chattingRoomQuery.getChattingRoomById(roomId);
-    List<User> users = userQuery.getUsersByRoom(chattingRoom);
-    List<ChattingMessage> allMessages = chattingMessageQuery.findAllMessages(chattingRoom);
-    backUpChattingRoom(allMessages, users, chattingRoom);
-    chattingRoomCommand.removeChattingRoom(chattingRoom);
-    sendChattingEndAlarm(users);
-    updateUserStatusToIdle(users);
-  }
+    List<User> chattingRoomUsers = userQuery.getUsersByRoom(chattingRoom);
+    chattingRoomUserValidator.validateUserInChattingRoom(requestUserId, chattingRoomUsers);
 
-  private void sendChattingEndAlarm(List<User> users) {
-    Set<NotificationInfo> notificationInfos = users.stream().map(User::getNotificationInfo)
+    ChattingRoomHistory chattingRoomHistory = chattingMigrationProcessor.backUpChattingRoom(
+        chattingRoom, chattingRoomUsers);
+    chattingMigrationProcessor.migrateChattingMessagesToHistoryInChattingRoom(chattingRoom,
+        chattingRoomHistory,
+        firstMessageId);
+    chattingMigrationProcessor.deleteChattingRoom(chattingRoom, chattingRoomUsers);
+
+    Set<NotificationInfo> notificationInfos = chattingRoomUsers.stream()
+        .map(User::getNotificationInfo)
         .collect(Collectors.toSet());
-    fcmNotificationSender.sendMultiNotifications(notificationInfos, CHATTING_END_MESSAGE);
-  }
-
-  private void backUpChattingRoom(List<ChattingMessage> allMessages, List<User> users,
-      ChattingRoom chattingRoom) {
-    ChattingRoomHistory chattingRoomHistory = chattingRoomHistoryCommand.createChattingRoomHistory(
-        chattingRoom);
-    chattingMessageHistoryCommand.createChattingMessageHistory(allMessages.stream().map(
-            chattingMessage -> new ChattingMessageHistory(chattingMessage.getMessage(),
-                chattingRoomHistory, chattingMessage.getCreatedAt(), chattingMessage.getUser()))
-        .toList());
-    userChattingHistoryCommand.createUserChattingHistory(
-        users.stream().map(user -> new UserChattingHistory(user, chattingRoomHistory)).toList());
-  }
-
-  private void updateUserStatusToIdle(List<User> users) {
-    users.forEach(User::setIdleStatus);
+    chattingRoomNotificationSender.notifyChattingRoomEnd(notificationInfos);
   }
 
   public ChattingRoomStatusDto checkChattingRoomStatus(Long roomId) {
     return ChattingRoomStatusDto.from(chattingRoomQuery.existsBy(roomId));
   }
+
 }
