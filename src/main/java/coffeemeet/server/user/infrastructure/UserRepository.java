de7a3420a72f362715428bc5be5432b366264db5
sangminnim <sangmins930@gmail.com>
2024-01-09 18:33:03 +0900

    refactor: 불필요한 쿼리메서드 삭제

--- src/main/java/coffeemeet/server/user/infrastructure/UserRepository.java
+++ src/main/java/coffeemeet/server/user/infrastructure/UserRepository.java
@@ -1,21 +1,25 @@
 package coffeemeet.server.user.infrastructure;
 
 import coffeemeet.server.chatting.current.domain.ChattingRoom;
-import coffeemeet.server.user.domain.OAuthInfo;
+import coffeemeet.server.user.domain.OAuthProvider;
 import coffeemeet.server.user.domain.User;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-  boolean existsUserByOauthInfo(OAuthInfo oAuthInfo);
+  @Query("""
+      SELECT u
+      FROM User u
+      WHERE u.oauthInfo.oauthProvider = :oAuthProvider AND u.oauthInfo.oauthProviderId = :oauthProviderId
+      """)
+  Optional<User> findByOauthInfo(OAuthProvider oAuthProvider, String oauthProviderId);
 
   Set<User> findByIdIn(Collection<Long> ids);
-
-  Optional<User> findByOauthInfo(OAuthInfo oauthInfo);
 
   boolean existsUserByProfile_Nickname(String nickname);
 
