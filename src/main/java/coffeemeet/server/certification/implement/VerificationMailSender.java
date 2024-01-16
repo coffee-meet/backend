8d8ec46cd0a82f497567999ffccf92b399e564e4
sangminnim <sangmins930@gmail.com>
2024-01-16 23:32:50 +0900

    feat: 이메일 전송 비동기 처리

--- src/main/java/coffeemeet/server/certification/implement/VerificationMailSender.java
+++ src/main/java/coffeemeet/server/certification/implement/VerificationMailSender.java
@@ -1,6 +1,7 @@
 package coffeemeet.server.certification.implement;
 
 import coffeemeet.server.certification.domain.CompanyEmail;
+import coffeemeet.server.common.domain.CoffeeMeetMail;
 import coffeemeet.server.common.infrastructure.EmailSender;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -9,14 +10,18 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class VerificationMailSender {
 
-  private static final String VERIFICATION_MAIL_SUBJECT = "[coffee-meet] 커피밋 사용을 위해 이메일 인증을 완료해주세요";
-  private static final String VERIFICATION_MAIL_BODY = "인증코드: %s";
+  private static final String VERIFICATION_MAIL_TITLE = "[coffee-meet] 커피밋 사용을 위해 이메일 인증을 완료해주세요";
+  private static final String VERIFICATION_MAIL_CONTENTS_FORM = "인증코드: %s";
 
   private final EmailSender emailSender;
 
   public void sendVerificationMail(CompanyEmail companyEmail, String verificationCode) {
-    emailSender.sendEmail(companyEmail.getValue(), VERIFICATION_MAIL_SUBJECT,
-        String.format(VERIFICATION_MAIL_BODY, verificationCode));
+    emailSender.sendMail(createVerificationMail(companyEmail.getValue(), verificationCode));
+  }
+
+  private CoffeeMeetMail createVerificationMail(String receiver, String code) {
+    String contents = String.format(VERIFICATION_MAIL_CONTENTS_FORM, code);
+    return new CoffeeMeetMail(receiver, VERIFICATION_MAIL_TITLE, contents);
   }
 
 }
