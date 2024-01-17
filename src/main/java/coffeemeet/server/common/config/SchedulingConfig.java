fd95006800c54d365516a55bf8d4402fe003db6e
박은지 <qkrdmswl1018@naver.com>
2024-01-17 12:34:55 +0900

    refactor: 스케쥴러 설정 변경

--- src/main/java/coffeemeet/server/common/config/SchedulingConfig.java
+++ src/main/java/coffeemeet/server/common/config/SchedulingConfig.java
@@ -2,19 +2,20 @@ package coffeemeet.server.common.config;
 
 import coffeemeet.server.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
-@Configuration
-@EnableScheduling
+@Slf4j
+@Component
 @RequiredArgsConstructor
 public class SchedulingConfig {
 
   private final UserService userService;
 
-  @Scheduled(cron = "0 3 * * *")
+  @Scheduled(cron = "0 0 3 * * *")
   public void checkUserDeleted() {
+    log.info("탈퇴 후 30일이 지난 회원 스케줄링 처리");
     userService.deleteUserInfos();
   }
 
