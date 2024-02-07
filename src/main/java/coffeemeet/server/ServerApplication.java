f7b4a7b75fdf9399bf35804c697d8b5c8749fb1f
박은지 <qkrdmswl1018@naver.com>
2024-02-07 10:10:56 +0900

    refactor: 스케줄러 설정 어노테이션 위치 변경

--- src/main/java/coffeemeet/server/ServerApplication.java
+++ src/main/java/coffeemeet/server/ServerApplication.java
@@ -2,10 +2,8 @@ package coffeemeet.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class ServerApplication {
 
   public static void main(String[] args) {
--- src/main/java/coffeemeet/server/common/config/ScheduleConfig.java
+++ src/main/java/coffeemeet/server/common/config/ScheduleConfig.java
@@ -3,13 +3,15 @@ package coffeemeet.server.common.config;
 import coffeemeet.server.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@EnableScheduling
 @RequiredArgsConstructor
-public class SchedulingConfig {
+public class ScheduleConfig {
 
   private final UserService userService;
 
